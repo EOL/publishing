1cab82a05c901643c8b78ef94def431c22b896d3
Huda Soliman <hudas@live.com>
2016-06-06 10:10:50 +0200

    display error msgs in openAuth#new

--- app/controllers/open_authentications_controller.rb
+++ app/controllers/open_authentications_controller.rb
@@ -1,16 +1,17 @@
 require 'devise'
 class OpenAuthenticationsController < ApplicationController
   def new
-    @user = User.new(email: params[:info][:email], password: Devise.friendly_token[0,16],
+    @user = User.new(email: params[:info][:email], password: Devise.friendly_token[0,16], 
                      username:  params[:info][:name])
     session[:new_user] = params
   end
 
   def create
      user = session[:new_user]
-     @user = User.new(email: params[:user][:email], password: Devise.friendly_token[0,16],
+     @user = User.new(email: params[:user][:email], password: Devise.friendly_token[0,16], 
                       username: user["info"]["name"])
      return render :new  unless @user.valid?
+
      if user["info"]["email"] != params[:user][:email]
        Devise::Mailer.confirmation_instructions(@user, @user.confirmation_token).deliver_now
        flash_msg = I18n.t(:signed_up_but_inactive, scope: 'devise.registrations')
@@ -21,8 +22,8 @@ class OpenAuthenticationsController < ApplicationController
 
      @user.save
      @user.after_confirmation
-     OpenAuthentication.create(user_id: @user.id,
-      provider: user["provider"], uid: user["uid"])
+     OpenAuthentication.create(user_id: @user.id, 
+       provider: user["provider"], uid: user["uid"])
      if user["info"]["email"] != params[:user][:email]
        redirect_to new_user_session_path, flash: { notice:  flash_msg }
      else
