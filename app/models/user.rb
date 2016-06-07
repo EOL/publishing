4b6df8f88c3757c9e2870ebf09432eea441f0afe
Huda Soliman <hudas@live.com>
2016-06-07 11:26:06 +0200

    fixing specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ActiveRecord::Base
 
   has_many :open_authentications, dependent: :delete_all
 
-  validates :username, presence: true,length: {minimum: 4, maximum: 32}
+  validates :username, presence: true, 
+   uniqueness: { case_sensitive: false },
+   length: { minimum: 4, maximum: 32 }
 
   def after_confirmation
     self.update_attributes(active: true)
