20529247a590730aab7890985c6c84e55bb986aa
Huda Soliman <hudas@live.com>
2016-06-09 14:58:02 +0200

    unfinished specs

--- spec/factories/open_authentications.rb
+++ spec/factories/open_authentications.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :open_authentication do
     sequence(:provider) { |n| "provider_#{n}" }
     sequence(:uid) {|n| "#{n}2345668" }
+    sequence(:user_id)  {n }
   end
 end  
