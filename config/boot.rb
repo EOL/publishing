fb5954de97dd60eeffbf23e9adf9d16759aad5f8
JRice <jrice@eol.org>
2016-06-24 13:05:56 -0400

    Just some comments

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,4 @@
+# This file is meant to set up Bundler and load paths. ONLY.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
