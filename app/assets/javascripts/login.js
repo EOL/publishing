efed2978dc77ead52d975ae57ada3dcaccf4230c
HudaSoliman <huda.soliman@bibalex.org>
2016-07-17 16:27:16 +0200

    shown

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,4 +1,4 @@
-var recaptchaError = false;
+var recaptchaError = false;var recaptchaChecked = false;
 var loginApp = angular.module('loginApp', []);
 loginApp.controller('loginValidate', function($scope, $window) {
     $scope.showErrors = false;
@@ -27,27 +27,52 @@ loginApp.controller('loginValidate', function($scope, $window) {
     };
 });
 
+loginApp.directive('validatePassword', function () {
+  return {
+    require: 'ngModel',
+    link: function (scope, element, attrs, ctrl) {
+      ctrl.$parsers.unshift(function(viewValue) {
+        scope.violatePwdLowerLimit = (viewValue && viewValue.length < 8 ? true : false);
+        scope.violatePwdUpperLimit = (viewValue && viewValue.length > 32 ? true : false);
+        if (!(scope.violatePwdLowerLimit || scope.violatePwdUpperLimit)){
+          scope.ViolatepwdLetterCond = (viewValue && /[A-Za-z]/.test(viewValue)) ? false : true;
+          scope.ViolatepwdNumberCond = (viewValue && /\d/.test(viewValue)) ? false : true;  
+        }
+        
+        if (scope.violatePwdLowerLimit && scope.violatePwdUpperLimit && scope.ViolatepwdLetterCond && scope.ViolatepwdNumberCond){
+          ctrl.$setValidity('pwd', false);
+          return viewValue;
+        } else {
+          ctrl.$setValidity('pwd', true);
+          return viewValue;
+        }
+      });
+    }
+  };
+});
 
-loginApp.directive('login', function($http) {
+
+loginApp.directive('validateEmail', function($http) {
   return {
     require: 'ngModel',
     link: function(scope, element, attr, mCtrl, http) {
-    	
+      
       function myValidation(value) {
-   //   	console.log("inside my validatin");
-      	console.log( element.val());
-      	console.log(scope.regex	);
-      	console.log(scope.email);
+   //     console.log("inside my validatin");
+        console.log( element.val());
+        console.log(scope.regex  );
+        console.log(scope.email);
+        
 //        $http({
  //         method : "GET",
  //         url : "/fetch_user?email=" + element.val(),
 
    //       }).then(function mySucces(response) {
           
-      //    	console.log(response + "success" + response.data);
+      //      console.log(response + "success" + response.data);
     //        mCtrl.$setValidity('email', response.data);
      //     }, function myError(response) {
-      //    	console.log(response + "error");
+      //      console.log(response + "error");
 
      //   });
       }
@@ -57,9 +82,10 @@ loginApp.directive('login', function($http) {
 });
 
 function recaptchaCallback() {
-    var appElement = document.querySelector('[ng-app=loginApp]');
+    var appElement = document.querySelector('[ng-app=signupApp]');
     var $scope = angular.element(appElement).scope().$$childHead;
     $scope.$apply(function() {
         $scope.recaptchaError = false;
+        $scope.recaptchaChecked = true;
     });
 }
