60e8e2d1924dcd7d283c81dc3fde52618210ff03
Huda Soliman <hudas@live.com>
2016-08-07 01:43:24 +0200

    close account angular

---
+++ app/assets/javascripts/materials.js
@@ -0,0 +1,19 @@
+var app = angular.module('confirmApp', ['ngMaterial']);
+app.controller('AppMaterials', function($scope, $mdDialog, $mdMedia, $http, $window) {
+  $scope.showConfirm = function(ev,user_id, msg) {
+    var confirm = $mdDialog.confirm()
+      .textContent(msg)
+      .targetEvent(ev)
+      .ok('ok')
+      .cancel('cancel');
+    $mdDialog.show(confirm).then(function() {
+      $http({
+        method : 'POST',
+        url :'/users/delete_user?id=' + user_id.toString()
+        }).then(
+         function mySucces(response){
+           $window.location.href = "http://"+ $window.location.host;
+        });
+      });
+    };
+});
