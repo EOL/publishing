63c5d4bb4b126e6fcc9401627b9d326de1b10016
JRice <jrice@eol.org>
2016-08-12 13:00:19 -0400

    One working spec for common name on page...

---
+++ spec/factories/node.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :node do
+    resource
+    page
+    sequence(:canonical_form) { |n| "<i>Scientificus nameri#{(n % 26 + 96).chr}</i>" }
+    sequence(:scientific_name) { |n| "#{canonical_form} Bergstrom #{1800 + n}" }
+    sequence(:resource_pk)
+
+    factory :native_node, class: "Node" do
+      resource { Resource.native }
+    end
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :user do
     sequence(:username) { |n| "username_#{n}" }
-    sequence(:email) {|n| "email_#{n}@example.com" }
+    sequence(:email) { |n| "email_#{n}@example.com" }
     password "password"
     admin false
   end
-  
+
   factory :admin_user, class: User do
     sequence(:username) { |n| "username_#{n}" }
     sequence(:email) {|n| "email_#{n}@example.com" }
@@ -14,4 +14,4 @@ FactoryGirl.define do
     confirmed_at Time.now
     admin true
   end
-end  
+end
