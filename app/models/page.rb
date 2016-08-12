171187b5d7628f6f76b28a8203ffb58605df98f4
JRice <jrice@eol.org>
2016-08-11 22:51:09 -0400

    Fixed another misnomer in the DB and fleshed out names for a page, but this is done.

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,9 +3,26 @@ class Page < ActiveRecord::Base
   belongs_to :moved_to_page, class_name: "Page"
 
   has_many :nodes, inverse_of: :page
-  has_many :page_contents, inverse_of: :page
+
   has_many :vernaculars, inverse_of: :page
+  has_many :preferred_vernaculars, -> { where(is_preferred: true) },
+    class_name: "Vernacular"
   has_many :scientific_names, inverse_of: :page
-  has_many :page_contents, inverse_of: :page
-  # TODO: content types... preferred vernacular... and much more!
+  has_one :name, -> { where(is_preferred: true) }, class_name: "ScientificName"
+
+  has_many :page_contents, -> { order(:position) }, as: :page
+  # TODO: test that the order is honored, here.
+  has_many :maps, through: :page_contents, source: :content, source_type: "Map"
+  has_many :articles, through: :page_contents, source: :content, source_type: "Article"
+  has_many :media, through: :page_contents, source: :content, source_type: "Medium"
+  has_many :links, through: :page_contents, source: :content, source_type: "Link"
+
+  scope :preloaded, -> do
+    includes(:name, :preferred_vernaculars)
+  end
+
+  def common_name(language = nil)
+    language ||= Language.english
+    preferred_vernaculars.find { |v| v.language_id == language.id }
+  end
 end
