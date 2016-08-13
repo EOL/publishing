0824ef8631d831078b1c56d357ef4a565e48b551
JRice <jrice@eol.org>
2016-08-13 16:45:39 -0400

    Code to grab the first article.

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,9 +2,37 @@ class Section < ActiveRecord::Base
   has_many :content_sections
   belongs_to :parent, class_name: "Section"
 
+  default_scope { order(:position) }
+
   has_many :content_sections
   has_many :contents, through: :content_sections
   has_many :children, class_name: "Section", foreign_key: "parent_id", inverse_of: :parent
 
   acts_as_list
+
+  class << self
+    def best_overviews
+      # TODO: we really should add positions to these... and creating them should
+      # all happen at once, not individually:
+      [ brief_summary, comprehensive_description, distribution ]
+    end
+
+    def brief_summary
+      @brief_summary ||= Section.where(name: "brief_summary").first_or_create do |s|
+        s.name = "brief_summary"
+      end
+    end
+
+    def comprehensive_description
+      @comprehensive_description ||= Section.where(name: "comprehensive_description").first_or_create do |s|
+        s.name = "comprehensive_description"
+      end
+    end
+
+    def distribution
+      @distribution ||= Section.where(name: "distribution").first_or_create do |s|
+        s.name = "distribution"
+      end
+    end
+  end
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,17 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Page do
+  before(:all) do
+    # TODO: this is problematic: class variables seem to become invalid between
+    # specs (if they are first called from within them) because of the
+    # first_or_create stuff. We'll have to have some kind of bootstrap to load
+    # into the DB... or add some code to clear class variables between specs
+    # (which sounds expensive).
+    Section.brief_summary
+  end
+
   context "with many common names" do
     let!(:our_page) { create(:page) }
     let!(:name1) { create(:vernacular, node: our_page.native_node) }
     let!(:pref_name) { create(:vernacular, node: our_page.native_node, is_preferred: true) }
     let!(:name2) { create(:vernacular, node: our_page.native_node) }
 
-    it "should select the preferred vernacular" do
+    it "selects the preferred vernacular" do
       expect(our_page.name).to eq(pref_name)
     end
 
-    it "should have access to all vernaculars" do
+    it "has access to all vernaculars" do
       expect(our_page.vernaculars).to include(name1)
       expect(our_page.vernaculars).to include(name2)
       expect(our_page.vernaculars).to include(pref_name)
@@ -26,14 +35,38 @@ RSpec.describe Page do
     end
     let!(:name2) { create(:scientific_name, node: our_page.native_node) }
 
-    it "should select the preferred scientific name" do
+    it "selects the preferred scientific name" do
       expect(our_page.scientific_name).to eq(pref_name)
     end
 
-    it "should have access to all scientific names" do
+    it "has access to all scientific names" do
       expect(our_page.scientific_names).to include(name1)
       expect(our_page.scientific_names).to include(name2)
       expect(our_page.scientific_names).to include(pref_name)
+    end
+  end
+
+  context "with a brief summary and another article" do
+    let!(:our_page) { create(:page) }
+    let!(:summary) do
+       article = create(:article)
+       ContentSection.create(content: article, section: Section.brief_summary)
+       PageContent.create(content: article, page: our_page, source_page: our_page)
+       article
+    end
+    let!(:other_article) do
+       article = create(:article)
+       PageContent.create(content: article, page: our_page, source_page: our_page)
+       article
+    end
+
+    it "chooses the summary for the overview" do
+      expect(our_page.top_articles.first).to eq(summary)
+    end
+
+    it "has access to both articles" do
+      expect(our_page.articles).to include(summary)
+      expect(our_page.articles).to include(other_article)
     end
   end
 end
