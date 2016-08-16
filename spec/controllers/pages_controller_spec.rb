2e47be692c9acc8f0d1e67a1ea8c8bbd07d28365
JRice <jrice@eol.org>
2016-08-16 14:51:31 -0400

    Added resource to the page ... uhhh... page.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,11 +2,24 @@ require 'rails_helper'
 
 RSpec.describe PagesController do
   let(:page) { create(:page) }
+  let(:resource) { create(:resource) }
+
+  before do
+    allow(TraitBank).to receive(:by_page) { [] }
+    allow(TraitBank).to receive(:resources) { [resource] }
+  end
 
   describe '#show' do
     it "assigns page" do
       get :show, id: page.id
       expect(assigns(:page)).to eq(page)
+    end
+  end
+
+  describe '#show' do
+    it "assigns resources" do
+      get :show, id: page.id
+      expect(assigns(:resources)).to eq([resource])
     end
   end
 end
