d91838ca310f131b1bbc0c4e09c9ecfc79046485
JRice <jrice@eol.org>
2016-08-19 09:13:42 -0400

    Seems to be working nicely, finally.

--- app/assets/javascripts/layout_search.js
+++ app/assets/javascripts/layout_search.js
@@ -1,15 +1,15 @@
 (function () {
   'use strict';
   angular
-    .module("searchApp", ["ngMaterial", "ui.bootstrap"])
+    .module("searchApp", ["ngMaterial", "ui.bootstrap", "ngSanitize"])
     .controller("SearchCtrl", SearchCtrl);
 
-  function SearchCtrl ($scope, $http) {
+  function SearchCtrl ($scope, $http, $window) {
     $scope.selected = undefined;
 
     $scope.querySearch = function(query) {
       return $http.get("/search.json", {
-        params: { q: query + "*" }
+        params: { q: query + "*", per_page: "6" }
       }).then(function(response){
         return response.data;
       });
@@ -17,7 +17,12 @@
 
     $scope.onSelect = function($item, $model, $label) {
       $scope.selected = $item.scientific_name;
-      console.log("Model: " + JSON.stringify($model));
-    }
+      // /pages/{{ match.model.id }}
+      $window.location = "/pages/" + $model.id;
+    };
+
+    $scope.nameOfModel = function($model) {
+      return $model.scientific_name.replace(/<\/?i>/g, "");
+    };
   };
 })();
