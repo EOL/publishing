ea3a72d4f08977a11e8665a7022c7a07d7babae7
JRice <jrice@eol.org>
2016-08-18 21:54:13 -0400

    WIP - Unstable, testing things, but want to save.

--- app/assets/javascripts/layout_search.js
+++ app/assets/javascripts/layout_search.js
@@ -1,41 +1,36 @@
 (function () {
   'use strict';
   angular
-    .module("searchApp", ['ngMaterial'])
-    .controller("SearchCtrl", SearchCtrl);
+    .module("searchApp", ["ngMaterial", "ngSanitize"])
+    .controller("SearchCtrl", SearchCtrl)
+    .filter("htmlSafe", function htmlSafe($sce) {
+      return function(val) {
+        console.log("I was here: " + $sce);
+        return $sce.trustAsHtml(val);
+      }
+    });
 
-  function SearchCtrl ($timeout, $q, $log, $window) {
+  function SearchCtrl ($timeout, $q, $log, $window, $http) {
     var self = this;
     self.querySearch = querySearch;
     self.selectedItemChange = selectedItemChange;
     self.searchTextChange = searchTextChange;
 
-    // TODO: change! I'm just testing, now:
-    // /search.json?q=raccoon
-    self.pages = [
-      {page: 328598, name: "procyon lotor", img: "http://media.eol.org/content/2014/06/14/12/40029_88_88.jpg"},
-      {page: 328598, name: "raccoon", img: "http://media.eol.org/content/2014/06/14/12/40029_88_88.jpg"},
-      {page: 19831, name: "raccoon dog", img: "http://media.eol.org/content/2012/05/23/20/56168_88_88.jpg"},
-      {page: 19831, name: "nyctereutes", img: "http://media.eol.org/content/2012/05/23/20/56168_88_88.jpg"}];
-
     function querySearch (query) {
-      var results = query ? self.pages.filter( createFilterFor(query) ) : self.pages, deferred;
-      deferred = $q.defer();
-      $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+      var deferred = $q.defer();
+      $http.get("/search.json?q=" + query + "*").then(function(response) {
+          // $log.info("Returned response: " + JSON.stringify(response.data));
+          deferred.resolve(response.data);
+      });
+      // return the promise
       return deferred.promise;
     };
     function searchTextChange(text) {
-      $log.info('Text changed to ' + text);
+      // $log.info('Text changed to ' + text);
     };
     function selectedItemChange(item) {
-      $log.info('Item changed to ' + JSON.stringify(item));
-      $window.location.href = "/pages/" + item.page;
-    };
-    function createFilterFor(query) {
-      var re = new RegExp("\\b" + angular.lowercase(query));
-      return function filterFn(page) {
-        return (re.exec(page.name));
-      };
+      // $log.info('Item changed to ' + JSON.stringify(item));
+      $window.location.href = "/pages/" + item.id;
     };
   };
 })();
