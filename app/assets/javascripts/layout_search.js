d6de1c68f542df13694bdb5f11fad8f4bfb4e01a
JRice <jrice@eol.org>
2016-08-18 22:56:57 -0400

    MOSTLY working... reload the page after using the search. Not sure why.

--- app/assets/javascripts/layout_search.js
+++ app/assets/javascripts/layout_search.js
@@ -1,36 +1,23 @@
 (function () {
   'use strict';
   angular
-    .module("searchApp", ["ngMaterial", "ngSanitize"])
-    .controller("SearchCtrl", SearchCtrl)
-    .filter("htmlSafe", function htmlSafe($sce) {
-      return function(val) {
-        console.log("I was here: " + $sce);
-        return $sce.trustAsHtml(val);
-      }
-    });
+    .module("searchApp", ["ngMaterial", "ui.bootstrap"])
+    .controller("SearchCtrl", SearchCtrl);
 
-  function SearchCtrl ($timeout, $q, $log, $window, $http) {
-    var self = this;
-    self.querySearch = querySearch;
-    self.selectedItemChange = selectedItemChange;
-    self.searchTextChange = searchTextChange;
+  function SearchCtrl ($scope, $http) {
+    $scope.selected = undefined;
 
-    function querySearch (query) {
-      var deferred = $q.defer();
-      $http.get("/search.json?q=" + query + "*").then(function(response) {
-          // $log.info("Returned response: " + JSON.stringify(response.data));
-          deferred.resolve(response.data);
+    $scope.querySearch = function(query) {
+      return $http.get("/search.json", {
+        params: { q: query + "*" }
+      }).then(function(response){
+        return response.data;
       });
-      // return the promise
-      return deferred.promise;
     };
-    function searchTextChange(text) {
-      // $log.info('Text changed to ' + text);
-    };
-    function selectedItemChange(item) {
-      // $log.info('Item changed to ' + JSON.stringify(item));
-      $window.location.href = "/pages/" + item.id;
-    };
+
+    $scope.onSelect = function($item, $model, $label) {
+      $scope.selected = $item.scientific_name;
+      console.log("Model: " + JSON.stringify($model));
+    }
   };
 })();
