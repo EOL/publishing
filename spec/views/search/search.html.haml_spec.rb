c94819e20e180287598c077ae40c5b9749d16f5b
JRice <jrice@eol.org>
2016-08-21 09:06:11 -0400

    Fixed search view spec...

--- spec/views/search/search.html.haml_spec.rb
+++ spec/views/search/search.html.haml_spec.rb
@@ -5,19 +5,35 @@ RSpec.describe "search/search" do
     before do
       name = instance_double("Vernacular", string: "a common name")
       lic = instance_double("License", name: "Image license name")
+      parent = instance_double("Node", ancestors: [], page_id: 342356,
+        canonical_form: "Ancestor name")
+      node = instance_double("Node", ancestors: [parent])
+      english = instance_double("English", code: "eng")
+      scientific_names = [instance_double("ScientificName",
+        canonical_form: "<i>Our scientific</i>")]
+      partner = instance_double("Partner", short_name: "Partner One")
+      resources = [instance_double("Resource", name: "Resource One",
+        partner: partner)]
       image1 = instance_double("Medium", license: lic, owner: "Owned Here",
         small_icon_url: "some_image_url_88_88.jpg", name: "Thumbnail Image")
       page = instance_double("Page", name: name, top_images: [image1],
-        scientific_name: "<i>Our scientific</i>")
+        scientific_name: scientific_names.first.canonical_form,
+        scientific_names: scientific_names, native_node: node,
+        vernaculars: [name], resources: resources)
       search_results = double("Sunspot::Search", results: [page])
       assign(:pages, search_results)
+      assign(:q, "common")
     end
 
-    it "shows the names" do
+    it "shows the scientific name" do
       render
       # note the titlecase:
-      expect(rendered).to match /A Common Name/
       expect(rendered).to match /Our scientific/
+    end
+
+    it "shows the highlighted common name in title case" do
+      render
+      expect(rendered).to match /A <b>Common<\/b> Name/
     end
 
     it "shows the icon" do
