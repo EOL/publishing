0e0ff3fae3cf600965cccd111bca629f61da48f4
JRice <jrice@eol.org>
2016-09-02 21:47:43 -0400

    Improved (but not fixed) specs by removing the new helper.

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe User::RegistrationsController, type: :controller do
   render_views
   describe 'check_captcha' do
     before do
-      request.env["devise.mapping"] = Devise.mappings[:user]
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      user_logged_in(nil)
     end
     let(:user) { create(:user) }
 
@@ -17,7 +18,8 @@ RSpec.describe User::RegistrationsController, type: :controller do
 
     context 'unverified recaptcha' do
       it "renders new action" do
-       allow(controller).to receive(:verify_recaptcha) { false }
+        # TODO: user_logged_in is not working here. :S
+        allow(controller).to receive(:verify_recaptcha) { false }
         post :create, user: { username: "user",
           email: "email_1@example.org", password: "password",
           password_confirmation: "password" }
