1e08bdc60e4218f1029837c0545b1e986f01ccb2
JRice <jrice@eol.org>
2016-09-07 07:11:40 -0400

    Improving coverage of collections controller

--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -3,25 +3,45 @@ require 'rails_helper'
 RSpec.describe CollectionsController do
   let(:user) { create(:user) }
   let(:page) { create(:page) }
-  let(:collection_attributes) do
-    attributes_for(:collection).
-      merge(collection_items_attributes:
-        { "0" => { item_id: page.id, item_type: page.class.to_s } })
+
+  before(:each) do
+    allow(controller).to receive(:current_user) { user }
   end
 
-  describe '#create (signed in)' do
-    before(:each) do
-      allow(controller).to receive(:current_user) { user }
-    end
+  context "with NO collection item" do
+    let(:collection) { build(:collection) }
 
-    it "redirects to collected item" do
-      post :create, collection: collection_attributes
-      expect(response).to redirect_to(page)
+    it "redirects to collection" do
+      post :create, collection: collection.attributes
+      created_collection = Collection.last
+      expect(response).to redirect_to(created_collection)
     end
 
     it "adds a flash message" do
-      post :create, collection: collection_attributes
-      expect(flash[:notice]).to match /#{page.collect_as}/
+      post :create, collection: collection.attributes
+      expect(flash[:notice]).to match /new collection "#{collection.name}"/
+    end
+  end
+
+  context "with a collection item" do
+    let(:collection_attributes) do
+      attributes_for(:collection).
+        merge(collection_items_attributes:
+          { "0" => { item_id: page.id, item_type: page.class.to_s } })
+    end
+
+    describe '#create (signed in)' do
+
+      it "redirects to collected item" do
+        post :create, collection: collection_attributes
+        expect(response).to redirect_to(page)
+      end
+
+      it "adds a flash message" do
+        post :create, collection: collection_attributes
+        expect(flash[:notice]).to match /new collection/
+        expect(flash[:notice]).to match /#{page.collect_as}/
+      end
     end
   end
 end
