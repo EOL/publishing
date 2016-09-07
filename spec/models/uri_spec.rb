9b03536e369c6bdf35150df866546de26a7b2c79
JRice <jrice@eol.org>
2016-09-07 07:26:34 -0400

    Spec for Uri class

---
+++ spec/models/uri_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Uri do
+  describe "#is_uri?" do
+    it "invokes URI::regexp for match" do
+      string = double(String)
+      expect(string).to receive(:=~).with(URI::regexp) { :that }
+      expect(Uri.is_uri?(string)).to eq(:that)
+    end
+  end
+end
