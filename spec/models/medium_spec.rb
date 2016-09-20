0d014732086f9dbfb28140b493340b7115b8696f
JRice <jrice@eol.org>
2016-09-20 14:10:13 -0400

    Guess what! ? MORE SPECS!

---
+++ spec/models/medium_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe Medium do
+  subject { Medium.new(base_url: "base") }
+
+  # # Drat. :S
+  # def name(language = nil)
+  #   self[:name]
+  # end
+
+  it "#name can take a language argument" do
+    expect(subject.name(Language.english)).to eq(subject.name)
+  end
+
+  it "builds a #original_size_url" do
+    expect(subject.original_size_url).to eq("base_original.jpg")
+  end
+
+  it "builds a #large_size_url" do
+    expect(subject.large_size_url).to eq("base_580_360.jpg")
+  end
+
+  it "builds a #medium_icon_url" do
+    expect(subject.medium_icon_url).to eq("base_130_130.jpg")
+  end
+
+  it "builds a #collect_with_icon" do
+    expect(subject.collect_with_icon).to eq("base_130_130.jpg")
+  end
+
+  it "builds an #icon" do
+    expect(subject.icon).to eq("base_130_130.jpg")
+  end
+
+  it "builds a #medium_size_url" do
+    expect(subject.medium_size_url).to eq("base_260_190.jpg")
+  end
+
+  it "builds a #small_size_url" do
+    expect(subject.small_size_url).to eq("base_98_68.jpg")
+  end
+
+  it "builds a #small_icon_url" do
+    expect(subject.small_icon_url).to eq("base_88_88.jpg")
+  end
+end
