66c4f4f3011ca673ae11d7866f9b1c20126a9ece
JRice <jrice@eol.org>
2016-09-20 13:47:52 -0400

    And yet more specs

--- spec/models/collected_page_spec.rb
+++ spec/models/collected_page_spec.rb
@@ -1,16 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe CollectedPage do
-  # before(:all) do
-  # end
-  # context "with many common names" do
-  #   let!(:our_page) { create(:page) }
-  #   let!(:name1) { create(:vernacular, node: our_page.native_node) }
-  #   let!(:pref_name) { create(:vernacular, node: our_page.native_node, is_preferred: true) }
-  #   let!(:name2) { create(:vernacular, node: our_page.native_node) }
-  #
-  #   it "selects the preferred vernacular" do
-  #     expect(our_page.name).to eq(pref_name.string)
-  #   end
-  # end
+  let(:page) { create(:page) }
+
+  context "with NO overrides" do
+    subject { CollectedPage.new(page: page) }
+
+    it "#item returns the page, to duck-type with CollectionItem" do
+      expect(subject.item).to eq(page)
+    end
+
+    it "#name uses the page" do
+      expect(page).to receive(:name) { "thisName" }
+      expect(subject.name).to eq("thisName")
+    end
+
+    it "#scientific_name_string uses the page" do
+      expect(page).to receive(:scientific_name) { "thisSciName" }
+      expect(subject.scientific_name_string).to eq("thisSciName")
+    end
+
+    context "with a top_image on the page" do
+      let(:image) do
+        instance_double("Medium", medium_icon_url: "thisIconMed",
+          small_icon_url: "thisIconSm")
+      end
+
+      before do
+        allow(page).to receive(:top_image) { image }
+      end
+
+      it "#medium_icon_url uses the page's top_image" do
+        expect(subject.medium_icon_url).to eq("thisIconMed")
+      end
+
+      it "#icon uses the page's top_image's medium_icon_url" do
+        expect(subject.icon).to eq("thisIconMed")
+      end
+
+      it "#small_icon_url uses the page's top_image" do
+        expect(subject.small_icon_url).to eq("thisIconSm")
+      end
+    end
+  end
+
+  context "with overrides" do
+    let(:vernacular) { create(:vernacular) }
+    let(:scientific_name) { create(:scientific_name) }
+    let(:medium) { create(:medium) }
+
+    subject do
+      CollectedPage.new(page: page, vernacular: vernacular,
+        scientific_name: scientific_name, medium: medium)
+    end
+
+    it "#name uses the override" do
+      expect(subject.name).to eq(vernacular.string)
+    end
+
+    it "#scientific_name_string uses the override's canonical_form" do
+      expect(subject.scientific_name_string).to eq(scientific_name.canonical_form)
+    end
+
+    it "#medium_icon_url uses the override" do
+      expect(subject.medium_icon_url).to eq(medium.medium_icon_url)
+    end
+
+    it "#icon uses the the override's medium_icon_url" do
+      expect(subject.icon).to eq(medium.medium_icon_url)
+    end
+
+    it "#small_icon_url uses the the override" do
+      expect(subject.small_icon_url).to eq(medium.small_icon_url)
+    end
+  end
 end
