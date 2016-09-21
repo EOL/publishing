f94f786dc1b628df55be277ffb475e4af6b3fd96
JRice <jrice@eol.org>
2016-09-21 17:13:46 -0400

    WIP - move article, media, nice media links and page icons

--- app/controllers/page_icons_controller.rb
+++ app/controllers/page_icons_controller.rb
@@ -2,11 +2,12 @@ class PageIconsController < ApplicationController
   def create
     # TODO: permissions
     PageIcon.create(icon_params.merge(user_id: current_user.id))
+    redirect_to page_url(icon_params[:page_id])
   end
 
 private
 
   def icon_params
-    params.require(:page_id, :medium_id)
+    params.permit([:page_id, :medium_id])
   end
 end
