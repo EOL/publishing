6da2c9aebf15a9a75d39bca2de3e341bd0348eae
JRice <jrice@eol.org>
2016-09-29 13:59:27 -0400

    Controller specs sufficient. Not brilliant.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -14,14 +14,21 @@ RSpec.describe SearchController do
     allow(User).to receive(:search) { users }
   end
 
-  describe '#show' do
-    before { get :search, q: "query" }
-    it { expect(assigns(:pages)).to eq(pages) }
-    it { expect(assigns(:collections)).to eq(collections) }
-    it { expect(assigns(:media)).to eq(media) }
-    it { expect(assigns(:users)).to eq(users) }
-    it { expect(assigns(:empty)).to eq(true) }
-    it { expect(assigns(:q)).to eq("query") }
-    it { expect(Page).to have_received(:search) }
+  describe "#show" do
+    context "when requesting all results" do
+      before { get :search, q: "query" }
+      it { expect(assigns(:pages)).to eq(pages) }
+      it { expect(assigns(:collections)).to eq(collections) }
+      it { expect(assigns(:media)).to eq(media) }
+      it { expect(assigns(:users)).to eq(users) }
+      it { expect(assigns(:empty)).to eq(true) }
+      it { expect(assigns(:q)).to eq("query") }
+      it { expect(Page).to have_received(:search) }
+    end
+    
+    context "when only requesting pages" do
+      before { get :search, q: "query", only: "pages" }
+      it { expect(Collection).not_to have_received(:search) }
+    end
   end
 end
