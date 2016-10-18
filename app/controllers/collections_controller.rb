93130913d74bf79e99cfb05ef2ec6978ea89e08a
JRice <jrice@eol.org>
2016-10-18 18:33:40 -0400

    WIP - Attempting to fix collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,4 @@
-class CollectionsController < ApplicationController
+  class CollectionsController < ApplicationController
   layout "collections"
 
   before_filter :find_collection_with_pages, only: [:edit, :show]
@@ -60,6 +60,6 @@ class CollectionsController < ApplicationController
   def collection_params
     params.require(:collection).permit(:name, :description,
       collection_associations_attributes: [:associated_id],
-      collected_pages_attributes: [:id, :page_id, collected_pages_media_attributes: [:medium_id]])
+      collected_pages_attributes: [:id, :page_id, collected_pages_media_attributes: [medium_ids: []]])
   end
 end
