e2d4019d4f7342fbe105224e3f50ae5c9bbe87e2
JRice <jrice@eol.org>
2016-10-18 18:33:40 -0400

    WIP - Attempting to fix collections

--- app/models/collected_page.rb
+++ app/models/collected_page.rb
@@ -10,7 +10,7 @@ class CollectedPage < ActiveRecord::Base
 
   acts_as_list scope: :collection
 
-  accepts_nested_attributes_for :collected_pages_media
+  accepts_nested_attributes_for :collected_pages_media, allow_destroy: true
 
   # For convenience, this is duck-typed from CollectionAssociation (q.v.)
   def item
