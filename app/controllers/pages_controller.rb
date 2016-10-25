49bcf91b4669cb82aa994853c41434c04321418e
JRice <jrice@eol.org>
2016-10-25 14:09:07 -0400

    Better 404, no maps as first image on import

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def show
     @page = Page.where(id: params[:id]).preloaded.first
+    raise "404" unless @page
     @resources = TraitBank.resources(@page.traits)
   end
 end
