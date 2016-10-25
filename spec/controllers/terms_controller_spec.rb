9ce63fb1588c554c4be3dcf7389ee38000af76dd
JRice <jrice@eol.org>
2016-10-25 14:48:38 -0400

    Fixing the term controller specs

--- spec/controllers/terms_controller_spec.rb
+++ spec/controllers/terms_controller_spec.rb
@@ -1,19 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe TermsController do
-  let(:term) { { name: "nom du term", uri: "http://a/term" } }
+  let(:trait) do
+    { :resource_pk=>"d7cf04b725e0c2f140489261ef365952", :source=>"Procyonidae",
+      :metadata=>nil, :resource_id=>17, :predicate=>{:section_ids=>"",
+      :name=>"extinction status", :attribution=>"",
+      :is_hidden_from_glossary=>false, :comment=>"", :definition=>"Indicates
+      whether a taxon is extant (living today) or extinct.",
+      :is_hidden_from_overview=>false,
+      :uri=>"http://eol.org/schema/terms/ExtinctionStatus"},
+      :object_term=>{:section_ids=>"", :name=>"extant",
+      :attribution=>"http://en.wikipedia.org/wiki/Extant_taxon",
+      :is_hidden_from_glossary=>false, :definition=>"This taxon is still in
+      existence, as opposed to extinct.", :comment=>"",
+      :is_hidden_from_overview=>false,
+      :uri=>"http://eol.org/schema/terms/extant"},
+      :id=>"trait:17:d7cf04b725e0c2f140489261ef365952"
+    }
+  end
   let(:resource) { create(:resource) }
   let(:page_measured) { create(:page) }
   let(:page_term) { create(:page) }
   let(:page_literal) { create(:page) }
-  let(:units) { { name: "units", uri: "http://a/unit" } }
-  let(:object_term) { { name: "term", uri: "http://a/term" } }
+  let(:units) { { uri: "http://units.com/unit/inch", name: "inch",
+    definition: "inching" } }
+  let(:term) { { uri: "http://terms.com/size/big", name: "big",
+    definition: "large" } }
   let(:glossary) { { units[:uri] => units, term[:uri] => term } }
   let(:grouped_traits) do
-    [ { page_id: page_measured.id, measurement: "657", units: units,
+    [ { page_id: page_measured.id, measurement: "657", units: units[:uri],
         resource_id: resource.id },
-      { page_id: page_term.id, term: term, resource_id: resource.id },
-      { page_id: page_literal.id, literal: "literal term value",
+      { page_id: page_term.id, term: term[:uri], resource_id: resource.id },
+      { page_id: page_literal.id, literal: "literal trait value",
         resource_id: resource.id }
     ]
   end
@@ -23,17 +41,19 @@ RSpec.describe TermsController do
     allow(TraitBank).to receive(:by_predicate) { grouped_traits }
     allow(TraitBank).to receive(:glossary) { glossary }
     allow(TraitBank).to receive(:resources) { [resource] }
-    allow(TraitBank).to receive(:term_as_hash) { term }
-    get :show, id: term[:uri]
   end
 
   describe '#show' do
-    it { expect(assigns(:term)).to eq(term) }
-    it { expect(assigns(:grouped_traits)).to eq(grouped_traits) }
+    before do
+      get :show, uri: trait[:predicate][:uri]
+    end
+
+    it { expect(assigns(:term)[:uri]).to eq(trait[:predicate][:uri]) }
     it { expect(assigns(:glossary)).to eq(glossary) }
     it { expect(assigns(:resources)).to eq([resource]) }
+    it { expect(assigns(:grouped_traits)).to eq(grouped_traits) }
 
-    it "assigns all pages" do
+    it "assigns pages" do
       pages.each do |page|
         expect(assigns(:pages).values).to include(page)
       end
