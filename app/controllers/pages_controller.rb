12e579c00539e99d9160ccda6c43b334f185d35d
JRice <jrice@eol.org>
2016-10-26 17:45:34 -0400

    Improved imports (associations show up, no images)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def show
     @page = Page.where(id: params[:id]).preloaded.first
     raise "404" unless @page
+    @media = @page.media.page(params[:page])
     @resources = TraitBank.resources(@page.traits)
   end
 end
