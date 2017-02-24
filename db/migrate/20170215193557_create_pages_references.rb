ca296579aa4c707153cc812caee32d42d79d90f5
JRice <jrice@eol.org>
2017-02-24 09:47:54 -0500

    Ooops! Coments in a migration failing tests

--- db/migrate/20170215193557_create_pages_references.rb
+++ db/migrate/20170215193557_create_pages_references.rb
@@ -1,27 +1,27 @@
 class CreatePagesReferences < ActiveRecord::Migration
   def change
-    # rename_table :references, :referents
-    # rename_table :articles_references, :references
-    # add_column :references, :parent_type, :string, default: "Article", null: false
-    # remove_index :references, column: :article_id
-    # rename_column :references, :article_id, :parent_id
-    # rename_column :references, :reference_id, :referent_id
-    # reversible do |dir|
-    #   change_table :references do |t|
-    #     dir.up   do
-    #       t.change :parent_type, :string, default: nil, null: false
-    #     end
-    #     dir.down do
-    #       # No need to do anthing.
-    #     end
-    #   end
-    # end
-    # add_index(:references, [:parent_type, :parent_id],
-    #   name: "references_by_parent_index")
-    # create_join_table(:pages, :referents) do |t|
-    #   t.index :page_id
-    #   t.integer :position
-    # end
+    rename_table :references, :referents
+    rename_table :articles_references, :references
+    add_column :references, :parent_type, :string, default: "Article", null: false
+    remove_index :references, column: :article_id
+    rename_column :references, :article_id, :parent_id
+    rename_column :references, :reference_id, :referent_id
+    reversible do |dir|
+      change_table :references do |t|
+        dir.up   do
+          t.change :parent_type, :string, default: nil, null: false
+        end
+        dir.down do
+          # No need to do anthing.
+        end
+      end
+    end
+    add_index(:references, [:parent_type, :parent_id],
+      name: "references_by_parent_index")
+    create_join_table(:pages, :referents) do |t|
+      t.index :page_id
+      t.integer :position
+    end
     reversible do |dir|
       dir.up do
         # NOTE: you cannot use find_each here because this table has no PK. This is
