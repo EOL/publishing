1910e0a94ff4cf202aa72ea1b5ab9d67964981e5
JRice <jrice@eol.org>
2017-03-28 11:00:49 -0400

    Legitimately test vernaculars on node

--- spec/models/node_spec.rb
+++ spec/models/node_spec.rb
@@ -5,17 +5,33 @@ RSpec.describe Node do
     # let(:node) { create(:node) }
     # it "has scientific name" do
       # expect(node.name).to eq(node.scientific_name)
-    # end 
+    # end
   # end
-   
+
   context "with vernaculars" do
-    let(:ver) { create(:vernacular, is_preferred: true) }
-    
+    let!(:node) { create(:node) }
+    let!(:ver) { create(:vernacular, node: node, is_preferred: true) }
+    let!(:lang) { create(:language, group: "de") }
+    let!(:lang_name) { create(:vernacular, node: node, language: lang,
+      is_preferred: true) }
+
     it "uses preloaded preferred vernaculars" do
-      expect(ver.node.name).to eq(ver.string)
+      expect(node.name).to eq(ver.string)
     end
-  
-    
-   
+
+    context "in another language" do
+      before do
+        @old_locale = I18n.locale
+        I18n.locale = :de
+      end
+
+      after do
+        I18n.locale = @old_locale
+      end
+
+      it "gets the right vernacular for language" do
+        expect(node.name).to eq(lang_name.string)
+      end
+    end
   end
 end
