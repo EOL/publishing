7e3a6f1ed26d2a036bff2007bdac41515d7711ab
JRice <jrice@eol.org>
2017-04-28 16:33:19 -0400

    Fix a migration--should NOT update Solr while running its cleanup task.

--- db/migrate/20170428193945_add_medium_id_to_pages.rb
+++ db/migrate/20170428193945_add_medium_id_to_pages.rb
@@ -1,10 +1,13 @@
+# 20170428193945
 class AddMediumIdToPages < ActiveRecord::Migration
   def change
     add_column(:pages, :medium_id, :integer,
       comment: "denormalized id of the top medium for this page: the icon.")
     # WARNING: this will be quite slow with a large database. Sorry.
+    Sunspot.session = Sunspot::Rails::StubSessionProxy.new(Sunspot.session)
     Page.all.select(:id).find_each do |page|
       page.update_attribute(:medium_id, page.media.first.id) unless page.media.empty?
     end
+    Sunspot.session = Sunspot.session.original_session
   end
 end
