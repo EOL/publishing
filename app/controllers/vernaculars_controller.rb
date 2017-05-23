bd6fe5d0f1a606cab5482534d80d73093b1320b0
JRice <jrice@eol.org>
2017-05-23 14:39:21 -0400

    VERY very very very basic way to select preferred common name.

---
+++ app/controllers/vernaculars_controller.rb
@@ -0,0 +1,12 @@
+class VernacularsController < ApplicationController
+  # TODO: permissions
+  def prefer
+    name = Vernacular.find(params[:id])
+    Vernacular.transaction do
+      name.page.vernaculars.where(language_id: name.language_id).update_all(is_preferred: false)
+      name.update_attribute(:is_preferred, true)
+    end
+    flash[:notice] = I18n.t("names.preferred_in_language_for_page", language: I18n.t("languages.#{name.language.group}"), page: name.page.scientific_name, name: name.string)
+    redirect_to(page_names_path(name.page))
+  end
+end
