ddee4008d126f80cddb84a69b5ccd6f9245cd2ed
JRice <jrice@eol.org>
2017-05-24 15:49:41 -0400

    Added staging env, changed around some configs as a result

---
+++ config/environments/staging.rb
@@ -0,0 +1,16 @@
+Rails.application.configure do
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
+  # TODO: set up mailing...
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025}
+  config.active_support.deprecation = :log
+  config.active_record.migration_error = :page_load
+  config.assets.debug = false
+  config.assets.digest = true
+  config.assets.raise_runtime_errors = true
+end
