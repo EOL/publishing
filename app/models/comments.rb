a8e0e16071a5477a8b431593292e56a3bf7cf8a8
JRice <jrice@eol.org>
2017-07-28 10:58:58 -0400

    Ooops, fixing discourse URL...

--- app/models/comments.rb
+++ app/models/comments.rb
@@ -2,16 +2,20 @@
 class Comments
   def self.discourse
     return @client if @client
-    @discourse_url = Rails.application.secrets.discourse_url
-    return Rails.logger.error("Missing discourse_url") if @discourse_url.nil?
+    discourse_url
+    return Rails.logger.error("Missing discourse_url") if discourse_url.nil?
     return Rails.logger.error("Missing discourse_url") if
       Rails.application.secrets.discourse_key.nil?
     return Rails.logger.error("Missing discourse_url") if
       Rails.application.secrets.discourse_user.nil?
     @client = DiscourseApi::Client.new(
-      @discourse_url,
+      discourse_url,
       Rails.application.secrets.discourse_key,
       Rails.application.secrets.discourse_user
     )
+  end
+
+  def self.discourse_url
+    @discourse_url ||= Rails.application.secrets.discourse_url
   end
 end
