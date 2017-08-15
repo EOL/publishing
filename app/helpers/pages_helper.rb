010a320e8ffe764d0fdc7e6e36707b215205abf3
JRice <jrice@eol.org>
2017-08-15 16:47:09 -0400

    First attempt. Yuck.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -142,7 +142,7 @@ module PagesHelper
   def index_stat(key, count)
     count = count > 1_000_000 ? "#{(count / 100_000) / 10.0}M" :
       number_with_delimiter(count)
-    haml_tag("div.ui.orange.statistic") do
+    haml_tag("div.ui.orange.statistic.uk-container-center") do
       haml_tag("div.value") { haml_concat count }
       haml_tag("div.label") { haml_concat t("landing_page.stats.#{key}") }
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,6 @@ u.activate
 user = User.create(email: "foo2@bar.com", username: "cigarman", name: "Sigmond Freud", password: "foobarbaz")
 user = User.create(email: "foo3@bar.com", username: "sweaver", name: "Sigourney Weaver", password: "foobarbaz")
 user = User.create(email: "foo@bar.com", username: "david", name: "David Attenboro", password: "foobarbaz")
+
+c = Collection.create(id: 1, name: "Featured Collections", description: "Items in this collection will be featured on the homepage.")
+c.users << u
