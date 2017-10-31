1ab4775cc9f5a69337ce69612452400753ae7999
Michael Vitale <mmvitale@gmail.com>
2017-10-31 15:00:33 -0400

    clean up search_helper, move filter type to decorated collection

--- app/decorators/kaminari_decorator.rb
+++ app/decorators/kaminari_decorator.rb
@@ -1,3 +1,7 @@
 class KaminariDecorator < Draper::CollectionDecorator
   delegate :current_page, :total_pages, :limit_value, :entry_name, :total_count, :offset_value, :last_page?
+
+  def type
+    decorated_collection.first&.type
+  end
 end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,70 +1,4 @@
 module SearchHelper
-  def page_common_name(page)
-    page.try(:search_highlights).try(:[], :preferred_vernacular_strings) || page.name
-  end
-
-  def page_sci_name(page)
-    page.try(:search_highlights).try(:[], :scientific_name) || page.scientific_name
-  end
-
-  def medium_name(medium)
-    medium.try(:search_highlights).try(:[], :name) || medium.name
-  end
-
-  def medium_owner(medium)
-    medium.try(:search_highlights).try(:[], :owner) || medium.owner
-  end
-
-  def medium_title(medium)
-    title = medium_name(medium)
-    type = medium_type(medium)
-
-    if !title.blank?
-      if type
-        I18n.t("search_results.medium_title_#{type}_html", :title => title)
-      else
-        title
-      end
-    else
-      if type
-        I18n.t("search_results.medium_#{type}")
-      else
-        I18n.t("search_results.no_title")
-      end
-    end
-  end
-
-  def user_name(user)
-    user.try(:search_highlights).try(:[], :username) || user.username
-  end
-
-  def user_bio(user)
-    user.try(:search_highlights).try(:[], :bio) || user.bio || ""
-  end
-
-  def collection_name(collection)
-    collection.try(:search_highlights).try(:[], :name) || collection.name
-  end
-
-  def collection_desc(collection)
-    if (!collection.users.empty?)
-      users_sentence = collection.users.map do |u|
-        u.username
-      end.to_sentence
-      I18n.t("search_results.collection_page_count_and_users", :count => collection.pages.length,
-        :users_sentence => users_sentence)
-    else
-      I18n.t("search_results.collection_page_count", :count => collection.pages.length)
-    end
-  end
-
-  def hierarchy_str(page)
-    ancestor_name = page_family_ancestor_name(page)
-    common_name = page_common_name(page)
-
-    ancestor_name ? "#{ancestor_name} … / #{common_name}" : common_name
-  end
-
   def result_total(results)
     total = 0
     results.each do |r|
@@ -83,26 +17,5 @@ module SearchHelper
     (collections && collections.total_count > 0 && :collections) ||
     (users && users.total_count > 0 && :users) ||
     nil
-  end
-
-private
-  def medium_type(medium)
-    return "article" if medium.is_a?(Article)
-    return nil if !medium.is_a?(Medium)
-    return "image" if medium.image?
-    return "video" if medium.video?
-    return "sound" if medium.sound?
-    return "map" if medium.map? || medium.js_map?
-  end
-
-  def page_family_ancestor_name(page)
-    ancestors = page.native_node.try(:ancestors)
-
-    return nil unless ancestors
-
-    ancestor = ancestors.detect do |a|
-      Rank.guess_treat_as(a.rank.name) === :r_family
-    end
-    ancestor ? ancestor.name : nil
   end
 end
