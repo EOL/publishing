9b7f1a0959a1b1a45e5b8433431e6256f6f239c0
JRice <jrice@eol.org>
2017-11-22 10:19:46 -0500

    Reverse order and paginate import logs

--- app/controllers/import_logs_controller.rb
+++ app/controllers/import_logs_controller.rb
@@ -1,5 +1,6 @@
 class ImportLogsController < ApplicationController
   def show
     @log = ImportLog.find(params[:id])
+    @events = @log.import_events.order("id DESC").page(params[:page]).per_page(50)
   end
 end
