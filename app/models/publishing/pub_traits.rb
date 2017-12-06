c566420218c9b23fedbb917846c8607a59ff342e
JRice <jrice@eol.org>
2017-12-06 12:26:44 -0500

    fixing assoc publish

--- app/models/publishing/pub_traits.rb
+++ app/models/publishing/pub_traits.rb
@@ -21,50 +21,12 @@ class Publishing::PubTraits
       target_page_id target_scientific_name value_uri value_literal value_num units]
     meta_rows << %i[eol_pk trait_resource_pk predicate value_literal value_num value_uri units sex lifestage
       statistical_method source]
-    @log.log('read_traits')
-    path = "resources/#{@resource.repository_id}/traits.json?"
-    @repo.loop_over_pages(path, "traits") do |trait|
-      row = []
-      trait_rows.first.each do |header|
-        row << trait[header]
-      end
-      trait_rows << row
-      meta = trait.delete(:metadata)
-      meta.each do |m_datum_camel|
-        meta_row = []
-        m_datum = Publishing::Repository.underscore_hash_keys(m_datum_camel)
-        meta_rows.first.each do |header|
-          if header == :trait_resource_pk
-            meta_row << trait[:resource_pk]
-          else
-            meta_row << m_datum[header]
-          end
-        end
-        meta_rows << meta_row
-      end
-    end
-    @log.log('read_associations')
-    path = "resources/#{@resource.repository_id}/assocs.json?"
-    @repo.loop_over_pages(path, "assocs") do |assoc|
-      row = []
-      trait_rows.first.each do |header|
-        row << assoc[header]
-      end
-      trait_rows << row
-      meta_row = []
-      meta = assoc_data.delete(:metadata)
-      meta_rows.first do |header|
-        if header == :trait_resource_pk
-          meta_row << assoc[:resource_pk]
-        else
-          meta_row << meta[header]
-        end
-      end
-      meta_rows << meta_row
-    end
+    read_data_type('traits', trait_rows, meta_rows)
+    read_data_type('assocs', trait_rows, meta_rows)
     return if trait_rows.size <= 1
-    @log.log("slurping traits and associations (#{trait_rows.size - 1}) and all metadata (#{meta_rows.size - 1}, "\
+    @log.log("slurping data (#{trait_rows.size - 1}) and all metadata (#{meta_rows.size - 1}, "\
       "total #{trait_rows.size + meta_rows.size - 2})")
+    # TODO: move these to Resource; delete the files after a successful publish.
     traits_file = Rails.public_path.join("traits_#{@resource.id}.csv")
     meta_traits_file = Rails.public_path.join("meta_traits_#{@resource.id}.csv")
     CSV.open(traits_file, 'w') { |csv| trait_rows.each { |row| csv << row } }
@@ -75,5 +37,34 @@ class Publishing::PubTraits
     @log.log("Keeping: #{meta_traits_file}.")
     # File.unlink(traits_file) if File.exist?(traits_file)
     # File.unlink(meta_traits_file) if File.exist?(meta_traits_file)
+  end
+
+  def grab_metadata(trait, meta_rows)
+    meta = trait.delete(:metadata)
+    meta.each do |m_datum_camel|
+      meta_row = []
+      m_datum = Publishing::Repository.underscore_hash_keys(m_datum_camel)
+      meta_rows.first.each do |header|
+        if header == :trait_resource_pk
+          meta_row << trait[:resource_pk]
+        else
+          meta_row << m_datum[header]
+        end
+      end
+      meta_rows << meta_row
+    end
+  end
+
+  def read_data_type(type, trait_rows, meta_rows)
+    @log.log("read_#{type}")
+    path = "resources/#{@resource.repository_id}/#{type}.json?"
+    @repo.loop_over_pages(path, type) do |datum|
+      row = []
+      trait_rows.first.each do |header|
+        row << datum[header]
+      end
+      trait_rows << row
+      grab_metadata(datum, meta_rows)
+    end
   end
 end
