022047feb027b4f7554eaf926635e06319d7e6eb
JRice <jrice@eol.org>
2017-12-10 13:28:11 -0500

    Comment for modifier names

--- app/helpers/data_helper.rb
+++ app/helpers/data_helper.rb
@@ -149,6 +149,7 @@ module DataHelper
 
   def show_data_modifiers(data)
     [data[:statistical_method], data[:sex], data[:lifestage]].compact.each do |type|
+      # TEMP - this should really be expressed with a relationship.
       if type =~ URI::ABS_URI
         type = TraitBank.term(type)[:name] rescue type
       end
