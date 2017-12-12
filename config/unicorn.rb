d8f9a37e0e1b341daeac7367a36394980306868f
Martin Taheri <M3hran@gmail.com>
2017-12-12 15:47:40 -0500

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,11 +2,11 @@ app_dir = "/u/apps/eol_website"
 
 working_directory app_dir
 
-pid "#{app_dir}/tmp/unicorn.pid"
+pid "#{app_dir}/tmp/pids/unicorn.pid"
 
 stderr_path "#{app_dir}/log/unicorn.stderr.log"
 stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
 worker_processes ENV["EOL_UNICORN_WORKER_PROCESSES"].to_i
-listen "/tmp/unicorn.sock", :backlog => 64
+listen "#{app_dir}/tmp/sockets/unicorn.sock", :backlog => 64
 timeout 240
