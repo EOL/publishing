81b3ca717ba6831ef31ae23790a37bb5463bd61b
JRice <jrice@eol.org>
2017-12-12 15:30:07 -0500

    Add CPKs for Percona's sake

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207180920) do
+ActiveRecord::Schema.define(version: 20171212202246) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "guid",                      limit: 255,        null: false
