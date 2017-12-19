2a13ee58197f83e7f88dd1a89289863276599641
JRice <jrice@eol.org>
2017-12-19 09:39:38 -0500

    Increase neography timeout:

--- config/initializers/neography.rb
+++ config/initializers/neography.rb
@@ -1,5 +1,5 @@
 Neography.configure do |config|
-  # Double the normal times: ...this isn't great for users, but it's great for admin. :S
-  config.http_send_timeout    = 2400
-  config.http_receive_timeout = 2400
+  # Quadruple the normal times: ...this isn't great for users, but it's great for admin. :S
+  config.http_send_timeout    = 4800
+  config.http_receive_timeout = 4800
 end
