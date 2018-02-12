8bcb50152c6ec2c7628174c9e078acae4c7384ec
JRice <jrice@eol.org>
2018-02-12 14:19:15 -0500

    Update MediaContentCreator to include Articles.

--- app/models/media_content_creator.rb
+++ app/models/media_content_creator.rb
@@ -17,43 +17,47 @@ class MediaContentCreator
 
   def by_resource
     @log.log('MediaContentCreator#by_resource', cat: :starts)
-    Medium.where(resource: @resource.id).where('page_id IS NOT NULL').find_in_batches do |batch|
-      reset_batch
-      learn_ancestry(batch)
-      batch.each do |medium|
-        add_content(medium.page_id, medium)
-        add_ancestry_content(medium)
+    [Medium, Article].each do |k|
+      @klass = k
+      @field = "#{@klass.name.underscore.downcase}_id".to_sym
+      @klass.where(resource: @resource.id).where('page_id IS NOT NULL').find_in_batches do |batch|
+        reset_batch
+        learn_ancestry(batch)
+        batch.each do |content|
+          add_content(content.page_id, content)
+          add_ancestry_content(content)
+        end
+        push_pages_down
+        import_contents
+        update_naked_pages if k == Medium
+        fix_counter_culture_counts
       end
-      push_pages_down
-      import_contents
-      update_naked_pages
-      fix_counter_culture_counts
     end
   end
 
   def learn_ancestry(batch)
     Page.includes(native_node: [:unordered_ancestors, { node_ancestors: :ancestor }])
-        .where(id: batch.map(&:page_id), medium_id: nil)
+        .where(id: batch.map(&:page_id))
         .each do |page|
-          @naked_pages[page.id] = page
+          @naked_pages[page.id] = page if page.send(@field).nil?
           @ancestry[page.id] = page.ancestry_ids
         end
   end
 
-  def add_content(page_id, medium)
+  def add_content(page_id, content)
     @content_count_by_page[page_id] ||= -1
     @content_count_by_page[page_id] += 1
     @contents << { page_id: page_id, source_page_id: page_id, position: @content_count_by_page[page_id],
-                   content_type: 'Medium', content_id: medium.id, resource_id: @resource.id }
+                   content_type: @klass.name, content_id: content.id, resource_id: @resource.id }
     if @naked_pages.key?(page_id)
-      @naked_pages[page_id].assign_attributes(medium_id: medium.id)
+      @naked_pages[page_id].assign_attributes(@field: content.id)
     end
   end
 
-  def add_ancestry_content(medium)
-    if @ancestry.key?(medium.page_id)
-      @ancestry[medium.page_id].each do |ancestor_id|
-        add_content(ancestor_id, medium) unless ancestor_id == medium.page_id
+  def add_ancestry_content(content)
+    if @ancestry.key?(content.page_id)
+      @ancestry[content.page_id].each do |ancestor_id|
+        add_content(ancestor_id, content) unless ancestor_id == content.page_id
       end
     end
   end
@@ -80,12 +84,12 @@ class MediaContentCreator
   def update_naked_pages
     unless @naked_pages.empty?
       @log.log("updating #{@naked_pages.values.size} pages with icons...")
-      Page.import!(@naked_pages.values, on_duplicate_key_update: [:medium_id])
+      Page.import!(@naked_pages.values, on_duplicate_key_update: [@field])
     end
   end
 
   def fix_counter_culture_counts
     @log.log("Fixing counter-culture counts...")
-    PageContent.where(content_type: 'Medium', content_id: @contents.map { |c| c[:content_id] }).counter_culture_fix_counts
+    PageContent.where(content_type: @klass.name, content_id: @contents.map { |c| c[:content_id] }).counter_culture_fix_counts
   end
 end
