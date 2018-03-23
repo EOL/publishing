88e9afa7aab15ced7845d9de924e2d3ddb3df9c3
Michael Vitale <mmvitale@gmail.com>
2018-03-23 16:59:09 -0400

    keep taxon info when expanding search result records

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -6,6 +6,7 @@ class DataController < ApplicationController
     @data = TraitBank.by_trait(params[:id]).first
     @resources = TraitBank.resources([@data])
     @page = Page.find(@data[:page_id])
+    @show_taxon = params[:show_taxon] && params[:show_taxon] == "true"
     render :layout => false
   end
 end
