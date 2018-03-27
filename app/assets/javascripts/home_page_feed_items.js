c48f052fb257e7022ec44f6ea718f59f7123ab34
Michael Vitale <mmvitale@gmail.com>
2018-03-27 17:16:05 -0400

    use rails ujs confirm mechanism for publish button

--- app/assets/javascripts/home_page_feed_items.js
+++ app/assets/javascripts/home_page_feed_items.js
@@ -1,5 +1,2 @@
-$(function() {
-  $('.js-publish').click(function(e) {
-    return confirm('Are you sure you want to publish this draft?');
-  });
-});
+// Put javascript for HomePageFeedItemsController pages here
+
