a930970ae9cedf12c1f0b4896c478de4afb8edb2
JRice <jrice@eol.org>
2018-04-09 09:56:46 -0400

    Undo

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -2,13 +2,7 @@ class HomePageController < ApplicationController
   before_action :no_main_container
 
   def index
-    @main_feed =
-      Rails.cache.fetch("home_pages/index/main_feed", expires_in: 10.minutes) do
-        HomePageFeed.find_by_name("main") || HomePageFeed.new
-      end
-    @partner_feed =
-      Rails.cache.fetch("home_pages/index/partner_feed", expires_in: 10.minutes) do
-        HomePageFeed.find_by_name("partner") || HomePageFeed.new
-      end
+    @main_feed = HomePageFeed.find_by_name("main") || HomePageFeed.new
+    @partner_feed = HomePageFeed.find_by_name("partner") || HomePageFeed.new
   end
 end
