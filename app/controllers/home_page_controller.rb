de3bd63f7b3a2b7b8c943d18f37945ddc4ab029e
JRice <jrice@eol.org>
2018-04-09 09:47:17 -0400

    10 min cache on homepage

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -2,7 +2,13 @@ class HomePageController < ApplicationController
   before_action :no_main_container
 
   def index
-    @main_feed = HomePageFeed.find_by_name("main") || HomePageFeed.new
-    @partner_feed = HomePageFeed.find_by_name("partner") || HomePageFeed.new
+    @main_feed =
+      Rails.cache("home_pages/index/main_feed", expires_in: 10.minutes) do
+        HomePageFeed.find_by_name("main") || HomePageFeed.new
+      end
+    @partner_feed =
+      Rails.cache("home_pages/index/partner_feed", expires_in: 10.minutes) do
+        HomePageFeed.find_by_name("partner") || HomePageFeed.new
+      end
   end
 end
