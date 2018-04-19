a7bfb588f7a06eee8047c2c78020a623e3cff9af
JRice <jrice@eol.org>
2018-04-19 10:35:31 -0400

    Moving TB schema to db/neo4j_schema.md

--- app/models/trait_bank.rb
+++ app/models/trait_bank.rb
@@ -1,37 +1,6 @@
-# Abstraction between our traits and the implementation of their storage. ATM, we
-# use neo4j.
-#
-# Please keep this schema summary comment in sync with the main
-# schema documentation file found in the doc/ directory, and reflect
-# and schema changes in the main documentation file.
-#
-# NOTE: in its current state, this is NOT done! Neography uses a plain hash to
-# store objects, and ultimately we're going to want our own models to represent
-# things. But in these early testing stages, this is adequate. Since this is not
-# its final form, there are no specs yet. ...We need to feel out how we want
-# this to work, first.
+# Abstraction between our traits and the implementation of their storage. ATM, we use neo4j. THE SCHEMA FOR TRAITS CAN
+# BE FOUND IN db/neo4j_schema.md ...please read that file before attempting to understand this one. :D
 class TraitBank
-  # NOTE: should associated pages (below, stored as object_page_id) actually
-  # have an association, since we have Pages? ...Yes, but only if that's
-  # something we're going to query... and I don't think we do! So all the info
-  # is reall in the MySQL DB and thus just the ID is enough.
-
-  # The Labels, and their expected relationships { and (*required) properties }:
-
-  # * Resource: { *resource_id }
-  # * Page: ancestor(Page)[NOTE: unused as of Nov2017], parent(Page), trait(Trait) { *page_id }
-  # * Trait: *predicate(Term), *supplier(Resource), metadata(MetaData), object_term(Term), units_term(Term),
-  #          normal_units_term(Term), sex_term(Term), lifestage_term(Term), statistical_method_term(Term),
-  #     { *eol_pk, *resource_pk, *scientific_name, source, measurement, object_page_id, literal, normal_measurement }
-  # * MetaData: *predicate(Term), object_term(Term), units_term(Term)
-  #     { *eol_pk, measurement, literal }
-  # * Term: parent_term(Term) { *uri, *name, *section_ids(csv), definition, comment,
-  #     attribution, is_hidden_from_overview, is_hidden_from_glossary, position,
-  #     type }
-
-  # NOTE: the "type" for Term is one of "measurement", "association", "value",
-  #   or "metadata" ... at the time of this writing.
-
   CHILD_TERM_DEPTH = 4
 
   class << self
---
+++ db/neo4j_schema.md
@@ -0,0 +1,43 @@
+# The Neo4j Schema
+Please keep this schema summary comment in sync with the main schema documentation file found in the doc/ directory, and
+reflect and schema changes in the main documentation file.
+
+### NOTE:
+in its current state, this is NOT done! Neography (a rails gem) uses a plain hash to store objects, and ultimately
+we're going to want our own models to represent things. But in these early testing stages, this is adequate. Since this
+is not its final form, there are no specs yet. ...We need to feel out how we want this to work, first.
+
+### NOTE:
+Should associated pages (below, stored as object_page_id) actually have an association, since we have Pages?
+...Yes, but only if that's something we're going to query... and I don't think we do! So all the info is really in the
+MySQL DB and thus just the ID is enough.
+
+## The Labels, and their expected relationships and properties:
+"Relationships" are only listed if they are outgoing. Attributes which are *italicized* are "required". Classes of
+relationships, if enforced, are preceded by a :Colon, and notes about either type are in [square braces].
+
+### Resource:
+* **Relationships**: none
+* **Attributes**: *resource_id*
+
+### Page:
+* **Relationships**: ancestor:Page[NOTE: unused as of Nov2017], parent:Page, trait:Trait
+* **Attributes**: *page_id*
+
+### Trait:
+* **Relationships**: *predicate*:Term, *supplier*:Resource, metadata:MetaData, object_term:Term, units_term:Term,
+  normal_units_term:Term, sex_term:Term, lifestage_term:Term, statistical_method_term:Term,
+* **Attributes**: *eol_pk*, *resource_pk*, *scientific_name*, source, measurement, object_page_id, literal,
+  normal_measurement
+
+### MetaData:
+* **Relationships**: *predicate*:Term, object_term:Term, units_term:Term
+* **Attributes**: *eol_pk*, measurement, literal
+
+### Term:
+* **Relationships**: parent_term:Term
+* **Attributes**: *uri*, *name*, *section_ids*[comma-separated], definition, comment, attribution,
+  is_hidden_from_overview, is_hidden_from_glossary, position, type }
+
+### NOTE:
+The "type" for Term is one of "measurement", "association", "value", or "metadata" ... at the time of this writing.
