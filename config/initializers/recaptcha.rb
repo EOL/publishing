e6ab7e8dba541d7934bddd41594d4445d9e23d4b
ranafayed <rana.fayed@bibalex.org>
2018-04-21 12:12:18 +0200

    removing some files

--- config/initializers/recaptcha.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-  config.site_key = '6Lf_900UAAAAAEi9P6SWT-9D2nuLZAz_tttz1joV'
-  config.secret_key = '6Lf_900UAAAAAFjtfbwCG-69I5KwfkoXE3lUV2hs'
-  config.proxy = "http://rana.fayed:R!5255726@wsa1:80"
-end
