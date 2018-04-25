d0ddc85175654447c4bd14b71221f0cd2bce6675
JRice <jrice@eol.org>
2018-04-25 11:08:57 -0400

    It helps if you don't keep your debugging queries in the code.

--- app/models/trait_bank.rb
+++ app/models/trait_bank.rb
@@ -362,58 +362,6 @@ class TraitBank
       end
     end
 
-query %q{MATCH (trait:Trait)<-[:trait]-(page:Page)-[:parent*]->(Page { page_id: 1642 }),
-(trait:Trait)-[:predicate]->(predicate:Term)-[:parent_term|:synonym_of*]->(tgt_pred:Term),
-(trait)-[:supplier]->(resource:Resource)
-WHERE tgt_pred.uri = "http://eol.org/schema/terms/Present"
-OPTIONAL MATCH (trait)-[:units_term]->(units:Term)
-OPTIONAL MATCH (trait)-[:normal_units_term]->(normal_units:Term)
-OPTIONAL MATCH (trait)-[:sex_term]->(sex_term:Term)
-OPTIONAL MATCH (trait)-[:lifestage_term]->(lifestage_term:Term)
-OPTIONAL MATCH (trait)-[:statistical_method_term]->(statistical_method_term:Term)
-OPTIONAL MATCH (trait)-[:object_term]->(object_term:Term)
-RETURN page, trait, predicate, units, normal_units, object_term, sex_term, lifestage_term, statistical_method_term, resource
-LIMIT 50
-}
-
-no_opts = query %q{MATCH (trait:Trait)<-[:trait]-(page:Page)-[:parent*]->(Page { page_id: 1642 }),
-(trait:Trait)-[:predicate]->(predicate:Term)-[:parent_term|:synonym_of*]->(tgt_pred:Term),
-(trait)-[:supplier]->(resource:Resource)
-WHERE tgt_pred.uri = "http://eol.org/schema/terms/Present"
-RETURN page, trait, predicate, resource
-LIMIT 1
-}
-
-sp_t = query %q{MATCH (trait:Trait { eol_pk: 'R96-PK12237234' })<-[:trait]-(page:Page)-[:parent*]->(Page { page_id: 1642 }),
-(trait:Trait)-[:predicate]->(predicate:Term)-[:parent_term|:synonym_of*]->(tgt_pred:Term),
-(trait)-[:supplier]->(resource:Resource)
-WHERE tgt_pred.uri = "http://eol.org/schema/terms/Present"
-RETURN page, trait, predicate, resource
-LIMIT 1
-}
-
-sp_t_no_clade = query %q{MATCH (trait:Trait { eol_pk: 'R96-PK12237234' })<-[:trait]-(page:Page),
-(trait:Trait)-[:predicate]->(predicate:Term)-[:parent_term|:synonym_of*]->(tgt_pred:Term),
-(trait)-[:supplier]->(resource:Resource)
-WHERE tgt_pred.uri = "http://eol.org/schema/terms/Present"
-RETURN page, trait, predicate, resource
-LIMIT 1
-} # no results
-
-sp_t = query %q{MATCH (trait:Trait { eol_pk: 'R96-PK12237234' })<-[:trait]-(page:Page)-[:parent*]->(Page { page_id: 1642 }),
-(trait:Trait)-[:predicate]->(predicate:Term)-[:parent_term|:synonym_of*]->(tgt_pred:Term),
-(trait)-[:supplier]->(resource:Resource)
-WHERE tgt_pred.uri = "http://eol.org/schema/terms/Present"
-RETURN page, trait, predicate, resource
-LIMIT 1
-}
-
-works = query %{MATCH (ancestor:Page { page_id: 1642 })<-[:parent*]-(page:Page)-[:trait]->(trait:Trait { eol_pk: 'R96-PK12237234' })-[:predicate]->(pred:Term) RETURN ancestor, page, trait, pred }
-
-works_sep = query %{MATCH (page:Page)-[:parent*]->(ancestor:Page { page_id: 1642 }), (page)-[:trait]->(trait:Trait { eol_pk: 'R96-PK12237234' }), (trait)-[:predicate]->(pred:Term) RETURN ancestor, page, trait, pred } # yup, fine.
-
-
-
     # TEMP: I'm skippping clade for count on the first page. This yields the wrong result, but speeds things up x2 ...
     # for the first page.
     def term_record_search(term_query, options)
