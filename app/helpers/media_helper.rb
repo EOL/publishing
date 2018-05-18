a2f439b2d3f1ed47fea24f87e313231f08a8f115
Michael Vitale <mmvitale@gmail.com>
2018-05-18 13:51:26 -0400

    fix 'this media is not in...'

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -2,4 +2,8 @@ module MediaHelper
   def image_owner(image)
     image.owner.html_safe.sub(/^\(c\)\s+/i, "").sub(/^&copy;\s+/i, "").html_safe
   end
+
+  def medium_not_in_collection_text(medium)
+    t("content_not_in_any_collection_#{medium.subclass}") 
+  end
 end
