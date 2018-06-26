10954e722339f1be03cf250be04c33d811354c1b
JRice <jrice@eol.org>
2018-06-26 10:17:25 -0400

    Ooops, forgot a <

--- app/models/node/mover.rb
+++ app/models/node/mover.rb
@@ -1,10 +1,10 @@
 class Node
   class Mover
-    class < self
+    class << self
       # Assumes you are passing in a list of node_pks with their new page ids as a (single) value.
       def by_hash(resource, nodes_to_pages)
         # Update all of the nodes, duh
-        
+
         # Rebuild the ancestry
 
         # Update the denormalized page ids on identifiers, node_ancestors, pages, scientific_names, taxon_remarks, and
