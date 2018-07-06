54c75c99952c979e15a8adcb139156c14691f122
Michael Vitale <mmvitale@gmail.com>
2018-07-06 12:31:49 -0400

    fix preferred scientificic name prefetch; include query url as column header

--- app/models/trait_bank/page_download_writer.rb
+++ app/models/trait_bank/page_download_writer.rb
@@ -1,14 +1,14 @@
 module TraitBank::PageDownloadWriter
-  def self.to_arrays(hashes)
+  def self.to_arrays(hashes, url)
     pages = Page.where(:id => TraitBank::DownloadUtils.page_ids(hashes)).
-      includes(:native_node, :preferred_vernaculars)
+      includes(:preferred_scientific_names, :preferred_vernaculars)
     data = []
-    data << cols.keys
+    data << (cols.keys << url)
     hashes.each do |result|
       page_id = TraitBank::DownloadUtils.page_id(result)
       page = pages.find { |p| p.id == page_id }
       next if !page
-      sci_name = page.native_node.scientific_names.where(:is_preferred => true).first # TODO: fix this hack
+      sci_name = page.preferred_scientific_names.first
       row = []
       self.cols.each do |_, lamb|
         row << lamb[page, sci_name]
@@ -30,13 +30,14 @@ module TraitBank::PageDownloadWriter
   end
 
   def self.write(hashes, base_filename, url)
-    arrays = self.to_arrays(hashes)
-    path = TraitBank::DataDownload.path.join("pages_#{base_filename}.tsv")
+    arrays = self.to_arrays(hashes, url)
+    filename = "pages_#{base_filename}.tsv"
+    path = TraitBank::DataDownload.path.join(filename)
 
     CSV.open(path, "wb", :col_sep => "\t") do |csv|
       arrays.each { |row| csv << row }
     end
 
-    path
+    filename
   end
 end
