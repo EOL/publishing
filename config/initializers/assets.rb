f591ebc45f28da812cce00d5ffb43b6b3b729860
Michael Vitale <mmvitale@gmail.com>
2018-07-16 13:03:02 -0400

    eli's map working on canned data

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,16 @@ Rails.application.config.assets.precompile += %w( *.js application.css )
 Rails.application.config.assets.precompile += %w( refinery/ckeditor.css )
 Rails.application.config.assets.precompile += %w( ckeditor/ckeditor.js )
 
+# For leaflet maps
+%w( leaflet MarkerCluster MarkerCluster.Default Control.FullScreen Leaflet.NavBar Control.Loading ).each do |css_asset|
+  Rails.application.config.assets.precompile << "#{css_asset}.css"
+end
+
+# TODO: remove last entry (should be dynamically loaded json)
+%w( leaflet leaflet.markercluster Control.FullScreen Leaflet.NavBar freezable Control.Loading maps_leaflet 5169 ).each do |js_asset|
+  Rails.application.config.assets.precompile << "#{js_asset}.js"
+end
+
 # controller-specific assets
 %w( search pages terms home_page_feeds home_page_feed_items home_page media ).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.css"]
