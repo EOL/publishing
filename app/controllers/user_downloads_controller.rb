439b0b958c51414f443c339f20fe51b17de69673
JRice <jrice@eol.org>
2018-07-18 16:02:00 -0400

    404 for missing files

--- app/controllers/user_downloads_controller.rb
+++ app/controllers/user_downloads_controller.rb
@@ -1,6 +1,7 @@
 class UserDownloadsController < ApplicationController
   def show
     download = UserDownload.find(params[:id])
+    return render(status: :not_found) if download.filename.nil?
     file = TraitBank::DataDownload.path.join(download.filename)
     # TODO: we should steal the "nice" name from the view helper, and rename the file.
     send_file(file, filename: download.filename, type: "text/tab-separated-values")
