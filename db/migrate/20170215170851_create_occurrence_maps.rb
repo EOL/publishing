5f111f4ba97bd408e95ba87e79aed94c61cee37d
JRice <jrice@eol.org>
2018-07-18 16:45:52 -0400

    Move maps path to where they really live instead of the symlink

--- db/migrate/20170215170851_create_occurrence_maps.rb
+++ db/migrate/20170215170851_create_occurrence_maps.rb
@@ -3,7 +3,7 @@ class CreateOccurrenceMaps < ActiveRecord::Migration
     # NOTE: this is (arguably) denormalized from nodes, because we don't want to
     # *harvest* this content yet. A specific import script will need to be run
     # to get these data. It's assumed that if a record exists for this page, the
-    # JSON with the occurrence data will be in the "public/maps/%{page.id %
+    # JSON with the occurrence data will be in the "public/data/maps/%{page.id %
     # 100}" directory. Things will break mightily if it's missing.
     create_table :occurrence_maps do |t|
       t.integer :resource_id
