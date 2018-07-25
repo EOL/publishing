ac66d029ad6b03b837b7f7980c0aee1b728b2321
JRice <jrice@eol.org>
2018-07-25 15:49:24 -0400

    Wow, this is ridiculously complex. I'm not sure it's sustainable. Need to think on it,.

--- app/models/page/serializer.rb
+++ app/models/page/serializer.rb
@@ -9,10 +9,11 @@ class Page::Serializer
   def initialize(page)
     @page = page
     @pages_dir = Rails.root.join('public', 'data', 'pages')
-    Dir.mkdir(pages_dir.to_s, 0755) unless Dir.exist?(pages_dir.to_s)
-    @pages_dir = @pages_dir.join(@page.id)
-    Dir.mkdir(pages_dir.to_s, 0755) unless Dir.exist?(pages_dir.to_s)
+    log("Made pages data dir") if Dir.mkdir(@pages_dir, 0755) unless Dir.exist?(@pages_dir)
+    @pages_dir = @pages_dir.join(@page.id.to_s)
+    log("Made new page dir #{@page.id}") if Dir.mkdir(@pages_dir, 0755) unless Dir.exist?(@pages_dir)
     @filenames = []
+    @limit = 100
   end
 
   def filename_for(table)
@@ -23,47 +24,52 @@ class Page::Serializer
   # TODO: curations
   # TODO: Resource filter
   def store_clade
-    structure = {
-      pages: [
-        :occurence_map,
-        { nodes: [ :identifiers, :node_ancestors, :resource, :rank,
-                   { scientific_names: [ :resource, :taxonomic_status ],
-                     vernaculars: [ :language, :resource ],
-                     references: :referents } ] },
-        page_contents_FILTER_media: [
-          :image_info, :license, :resource, :language, :bibliographic_citation, :location, :sytlesheet, :javascript,
-            :attributions,
-            { content_sections: :section, references: :referents }
-        ],
-        page_contents_FILTER_articles: [
-          :license, :resource, :language, :bibliographic_citation, :location, :sytlesheet, :javascript, :attributions,
-            { content_sections: :section, references: :referents }
-        ]
-      ]
-    }
+    structure = [
+      :occurrence_maps, {
+        nodes: [ :identifiers, :node_ancestors, :resource, :rank,
+                 { scientific_names: [ :resource, :taxonomic_status ],
+                   vernaculars: [ :language, :resource ],
+                   references_AS_parent: :referents } ],
+        page_contents: [{
+          media_AS_content: [
+            # TODO: :sytlesheet, :javascript,
+            :image_infos, :license, :resource, :language, :bibliographic_citation, :location, # TODO: :attributions,
+              { content_sections: :section, references_AS_parent: :referents }
+          ],
+          articles_AS_content: [
+            # TODO: :sytlesheet, :javascript,
+            :license, :resource, :language, :bibliographic_citation, :location, # TODO: :attributions,
+              { content_sections: :section, references_AS_parent: :referents }
+          ]
+        }]
+      }
+    ]
 
-    node_ids = @page.native_node.descendants.pluck(:id)
-    node_ids << @page.native_node_id unless node_ids.include?(@page.native_node_id)
-    page_ids = Node.where(id: node_ids.pluck(:page_id)
+    node_ids = @page.native_node.descendants.pluck(:node_id)
+    node_ids << @page.native_node_id
+    page_ids = Node.where(id: node_ids).pluck(:page_id)
     @tables = { Page: page_ids }
-    structure.each do |relationship|
-      gather(:pages, page_ids, relationship)
-    end
+    structure.each { |relationship| gather(:pages, page_ids, relationship) }
+    # Write the tables as CSV to files...
     @filenames
   end
 
   def gather(source, source_ids, relationship)
     # YOU WERE HERE: You still need to handle "FILTER" in the names. ...At least for the first relationship.
-    source_class = Object.const_get(source.to_s.singularize.classify)
+    log("GATHER #{source}, (#{source_ids.size} ids), #{relationship.inspect}")
     if relationship.is_a?(Symbol) # e.g. :occurrence_map
-      gather_relationship_ids(source_class, source_ids, relationship)
+      log(".. SYMBOL")
+      gather_relationship_ids(source, source_ids, relationship)
     elsif relationship.is_a?(Array)
+      log(".. ARRAY")
       relationship.each do |specific_relationship|
-        gather_relationship_ids(source_class, source_ids, specific_relationship)
+        gather(source, source_ids, specific_relationship)
       end
     elsif relationship.is_a?(Hash)
+      log(".. HASH")
       relationship.each do |child, descendants|
-        children_ids = gather_relationship_ids(source_class, source_ids, child)
+        log(".. CHILD: #{child} ; DESC: #{descendants.inspect}")
+        children_ids = gather_relationship_ids(source, source_ids, child)
         gather(child, children_ids, descendants)
       end
     else
@@ -71,16 +77,52 @@ class Page::Serializer
     end
   end
 
-  def gather_relationship_ids(source_class, source_ids, relationship)
-    relationship_class = Object.const_get(relationship.to_s.singularize.classify)
+  def gather_relationship_ids(source, source_ids, relationship)
+    log("GATHER_REL_IDS #{source}, (#{source_ids.size} ids), #{relationship.inspect}")
+    relationship_class = objectify(relationship)
+    relationship_name = strip_name(relationship)
+    source_class = objectify(source)
     @tables[relationship_class] ||= []
-    new_ids +=
-      if relationship.to_s == relationship.to_s.singularize
-        source_class.where(id: source_ids).pluck("#{relationship}_id")
+    new_ids =
+      if relationship_name == relationship_name.singularize
+        log("(singular)")
+        source_class.where(id: source_ids).limit(@limit).pluck("#{relationship_name.singularize}_id")
       else
-        relationship_class.where("#{relationship}_id": source_ids).pluck(:id)
+        log("(plural)")
+        filter = { "#{source_class.name.underscore.singularize}_id" => source_ids }
+        parent_field = nil
+        if relationship.to_s =~ /_AS_(\w+)$/
+          log(".. SPECIAL 'AS' HANDLER")
+          parent_field = $1
+          filter = { "#{parent_field}_id" => source_ids }
+        end
+        # AUGH! This is hard. Content is polymorphic, so handle the case specially:
+        if parent_field == 'content'
+          log(".. SPECIAL CONTENT HANDLER")
+          source_class.where(id: source_ids).limit(@limit).pluck("content_id")
+        else
+          log(".. NORMAL PLURAL")
+          relationship_class.where(filter).limit(@limit).pluck(:id)
+        end
       end
+    new_ids.uniq!
     @tables[relationship_class] += new_ids
+    log(".. FOUND #{new_ids.size} new IDs")
     new_ids
+  end
+
+  def objectify(original_name)
+    name = strip_name(original_name)
+    Object.const_get(name.singularize.classify)
+  end
+
+  def strip_name(name)
+    name.to_s.sub(/_(FILTER|AS)_\w+$/, '')
+  end
+
+  def log(what)
+    # TODO: better logging. For now:
+    puts "** #{what}"
+    Rails.logger.info("[#{Time.now.strftime('%H:%M:%S.%3N')}] SERIALIZER: #{what}")
   end
 end
