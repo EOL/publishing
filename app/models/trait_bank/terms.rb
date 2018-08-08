be3e8169d939bb9d2195b4d6918b28c822ec95be
JRice <jrice@eol.org>
2018-08-08 12:26:38 -0400

    Rename cache for same.

--- app/models/trait_bank/terms.rb
+++ app/models/trait_bank/terms.rb
@@ -184,7 +184,7 @@ class TraitBank
       end
 
       def any_obj_terms_for_pred?(pred)
-        Rails.cache.fetch("trait_bank/any_obj_terms_for_pred/#{pred}", expires_in: CACHE_EXPIRATION_TIME) do
+        Rails.cache.fetch("trait_bank/pred_has_object_terms/#{pred}", expires_in: CACHE_EXPIRATION_TIME) do
           query(
             %{MATCH (term:Term)<-[:object_term]-(:Trait)-[:predicate]->(:Term)<-[:synonym_of|:parent_term*0..]-(:Term { uri: '#{pred}'}) RETURN term.uri LIMIT 1}
           )["data"].any?
