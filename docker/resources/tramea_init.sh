0daec7049a36ea5039311a34eac8f49be24d7b10
JRice <jrice@eol.org>
2018-08-14 14:00:06 -0400

    Fix some paths in configs

--- docker/resources/tramea_init.sh
+++ docker/resources/tramea_init.sh
@@ -5,7 +5,7 @@ echo "Updating $APP.."
 cd /u/apps/$APP && git pull
 
 cp /u/apps/secrets.yml /u/apps/$APP/config/secrets.yml
-/bin/bash -l -c 'cd /u/apps/eol_website/ && bundle'
-/bin/bash -l -c 'cd /u/apps/eol_website/ && rake log:clear'
-/bin/bash -l -c "cd /u/apps/eol_website && bundle exec unicorn -c /u/apps/eol_website/config/unicorn.rb -D"
+/bin/bash -l -c 'cd /app/ && bundle'
+/bin/bash -l -c 'cd /app/ && rake log:clear'
+/bin/bash -l -c "cd /app && bundle exec unicorn -c /app/config/unicorn.rb -D"
 service nginx start
