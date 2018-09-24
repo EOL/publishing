2e84b8f644a23e2e4d31ee92c508d5606479041d
Michael Vitale <mmvitale@gmail.com>
2018-09-24 14:13:26 -0400

    standardize blank medium name behavior

--- app/helpers/media_helper.rb
+++ app/helpers/media_helper.rb
@@ -6,4 +6,9 @@ module MediaHelper
   def medium_not_in_collection_text(medium)
     t("content_not_in_any_collection_#{medium.subclass}") 
   end
+
+  def medium_name_html(medium)
+    name = medium.name.blank? ? t("medium.untitled.#{medium.subclass}") : medium.name
+    name.sub(/^File:/, "").sub(/\.\w{3,4}$/, "").html_safe
+  end
 end
