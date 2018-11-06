65dacc3037bc1b001055d373d9e55260c1b4efde
JRice <jrice@eol.org>
2018-11-06 13:39:14 -0500

    Fix problem with articles in API

--- app/controllers/legacy_api_controller.rb
+++ app/controllers/legacy_api_controller.rb
@@ -19,9 +19,13 @@ protected
     object_hash[:rights]                 = object.rights_statement if object.rights_statement
     object_hash[:rightsHolder]           = object.owner if object.owner
     object_hash[:bibliographicCitation]  = object.bibliographic_citation.body if object.bibliographic_citation
-    object_hash[:source]                 = object.source_page_url if object.source_page_url
+    if object.respond_to?(:source_page_url)
+      object_hash[:source]                 = object.source_page_url if object.source_page_url
+      object_hash[:mediaURL]               = object.source_url if object.source_url
+    else
+      object_hash[:source]                 = object.source_url if object.source_url
+    end
     object_hash[:description]            = object.description if object.description
-    object_hash[:mediaURL]               = object.source_url if object.source_url
     if object.image?
       object_hash[:eolMediaURL]          = object.original_size_url
       object_hash[:eolThumbnailURL]      = object.small_size_url
