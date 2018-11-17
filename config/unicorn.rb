95972dd35c2bc3acf9a8533937128f3d5e78702b
JRice <jrice@eol.org>
2018-11-17 11:28:26 -0500

    Workers to 24

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,6 +7,6 @@ pid "#{app_dir}/tmp/unicorn.pid"
 stderr_path "#{app_dir}/log/unicorn.stderr.log"
 stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
-worker_processes 8
+worker_processes 24
 listen "#{app_dir}/tmp/unicorn.sock", :backlog => 64
 timeout 240
