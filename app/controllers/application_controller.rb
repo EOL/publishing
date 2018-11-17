73d3a88064a33df3452ef7f194ae54a35c81b51d
JRice <jrice@eol.org>
2018-11-17 10:14:26 -0500

    I did that totally wrong

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < ActionController::Base
 
   def route_not_found
     respond_to do |format|
-      format.html { render 'error_pages/404', status: :not_found }
-      format.all { redirect_to controller: 'application', action: 'route_not_found' }
+      format.png { redirect_to controller: 'application', action: 'route_not_found' }
+      format.all { render 'error_pages/404', status: :not_found }
     end
   end
 
