ad7ed4d80e8d1fa121f09cb45ec9d20bbfe02bcc
Michael Vitale <mmvitale@gmail.com>
2018-12-18 16:14:49 -0500

    require admin for errors controller

--- app/controllers/user_download/errors_controller.rb
+++ app/controllers/user_download/errors_controller.rb
@@ -1,4 +1,6 @@
 class UserDownload::ErrorsController < ApplicationController
+  before_action :require_admin
+
   def show
     @download = UserDownload.find(params[:download_id])
     return render(status: :not_found) if !@download.failed? || @download.download_error.nil?
