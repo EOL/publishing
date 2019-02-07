650aa0da9a2e0cf6461c1af710a38dc2ed9d2cca
Jonathan A Rees <jar398@mumble.net>
2019-02-07 10:26:59 -0500

    documentation and organizational improvements

--- lib/traits_dumper.rb
+++ lib/traits_dumper.rb
@@ -1,18 +1,12 @@
+# ID=7674 CHUNK=20000 TOKEN=`cat ../api.token` ZIP=felidae.zip ruby -r ./lib/traits_dumper.rb -e TraitsDumper.main
+# (7674 is Felidae)
+
 # Generate CSV files expressing trait information for a single taxon
 # recursively.
 
-# For the future, in case we decide to query by predicate:
-=begin
- MATCH (term:Term)
- WHERE term.type = "measurement"
- OPTIONAL MATCH (term)-[:parent_term]->(parent:Term) 
- WHERE parent.uri IS NULL
- RETURN term.uri, term.name
- LIMIT 1000
-=end
-
 require 'csv'
 require 'fileutils'
+require 'zip'
 
 # These are required if we want to be an HTTP client:
 require 'net/http'
@@ -57,10 +51,12 @@ class TraitsDumper
   end
 
   def doit
-    write_zip [emit_pages,
-               emit_traits,
-               emit_metadatas,
-               emit_terms]
+    predicates = list_predicates
+    STDERR.puts "#{predicates.length} predicate URIs"
+    write_zip [emit_terms,
+               emit_pages,
+               emit_traits(predicates),
+               emit_metadatas(predicates)]
   end
 
   # There is probably a way to do this without creating the temporary
@@ -91,7 +87,35 @@ class TraitsDumper
     end
   end
 
-  #---- Query #3: Pages
+  #---- Query: Predicates (subset of Terms)
+
+  def list_predicates
+    predicates_query =
+     'MATCH (term:Term {type: "measurement"})
+      RETURN term.uri
+      LIMIT 10000'
+    run_query(predicates_query)["data"]
+  end
+
+  #---- Query: Terms
+
+  def emit_terms
+
+    # Many Term nodes have 'uri' properties that are not URIs.  Would it 
+    # be useful to filter those out?  It's about 2% of the nodes.
+
+    # I'm not sure where there exist multiple Term nodes for a single URI?
+
+    terms_query =
+     "MATCH (r:Term)
+      OPTIONAL MATCH (r)-[:parent_term]->(parent:Term)
+      RETURN r.uri, r.name, r.type, parent.uri
+      ORDER BY r.uri"
+    terms_keys = ["uri", "name", "type", "parent_uri"]
+    supervise_query(terms_query, terms_keys, "terms.csv")
+  end
+
+  #---- Query: Pages (taxa)
 
   def emit_pages
     pages_query =
@@ -103,9 +127,9 @@ class TraitsDumper
     supervise_query(pages_query, pages_keys, "pages.csv")
   end
 
-  #---- Query #2: Traits
+  #---- Query: Traits (trait records)
 
-  def emit_traits
+  def emit_traits(predicates)
 
     traits_query =
      "MATCH (t:Trait)<-[:trait]-(page:Page)
@@ -136,9 +160,9 @@ class TraitsDumper
     supervise_query(traits_query, traits_keys, "traits.csv")
   end
 
-  #---- Query #1: Metadatas
+  #---- Query: Metadatas
 
-  def emit_metadatas
+  def emit_metadatas(predicates)
 
     metadata_query = 
      "MATCH (m:MetaData)<-[:metadata]-(t:Trait),
@@ -155,107 +179,104 @@ class TraitsDumper
     supervise_query(metadata_query, metadata_keys, "metadata.csv")
   end
 
-  #---- Query #0: Terms
-
-  def emit_terms
-
-    # Many Term nodes have 'uri' properties that are not URIs.  Would it 
-    # be useful to filter those out?  It's about 2% of the nodes.
-
-    # I'm not sure where there exist multiple Term nodes for a single URI?
-
-    terms_query =
-     "MATCH (r:Term)
-      OPTIONAL MATCH (r)-[:parent_term]->(parent:Term)
-      RETURN r.uri, r.name, r.type, parent.uri
-      ORDER BY r.uri"
-    terms_keys = ["uri", "name", "type", "parent_uri"]
-    supervise_query(terms_query, terms_keys, "terms.csv")
-  end
-
   # -----
 
-  # filename is relative to @csvdir
+  # supervise_query
+  # The purpose is to create a .csv file for a particular table (traits, pages, etc.).
+
+  # The result sets are too big to capture with a single query, due to
+  # timeouts or other problems, so the query is applied many times
+  # to get "chunks" of results.
+
+  # Each chunk is placed in a file.  If a chunk file already exists
+  # the query is not repeated - the results from the previous run are
+  # used directly without verification.
+
+  # filename (where to put the .csv file) is interpreted relative to @csvdir
 
   def supervise_query(query, columns, filename)
     path = File.join(@csvdir, filename)
     if File.exist?(path)
       STDERR.puts "reusing previously created #{path}"
     else
-      # Create a directory filename.parts to hold the parts
-      parts_dir = path + ".parts"
-
-      if @chunksize
-        limit_phrase = "LIMIT #{@chunksize}"
-      else
-        limit_phrase = ""
-      end
-
-      parts = []
-      skip = 0
-      while true
-        # Fetch it in parts
-        part = File.join(parts_dir, "#{skip}.csv")
-        if File.exist?(part)
-          STDERR.puts "reusing previously created #{part}"
-          parts.push(part)
-          # TBD: we should increase skip by the actual number of
-          # records in the file.
-          skip += @chunksize if @chunksize
-        else
-          result = query(query + " SKIP #{skip} #{limit_phrase}")
-          # A null result means that there was some kind of error, which
-          # has already been reported.  (because I don't want to learn
-          # ruby exception handling!)
-          got = result["data"].length
-          if result and got > 0
-            emit_csv(result, columns, part)
-            parts.push(part)
-          end
-          skip += got
-          break if @chunksize and got < @chunksize
-        end
-        break unless @chunksize
-      end
-
-      # Concatenate all the parts together
-      if parts.size == 0
-        nil
-      elsif parts.size == 1
-        FileUtils.mv parts[0], path
-        path
-      else
-        temp = path + ".new"
-        if false
-          # The 'tail' man page is wrong; it says the +2 should follow -q, but the
-          # command barfs if you do it that way.
-          # Also, the 'tail' command fails when there are lots of files.
-          # 'gtail' doesn't have this bug.  (could 'map' to work around.)
-          more_files = parts.drop(1).join(' ')
-          more = "tail +2 -q #{more_files}"
-        elsif false
-          # Use gnu 'tail' (might or might not be available under the name 'gtail')
-          more_files = parts.drop(1).join(' ')
-          more = "gtail -n +2 -q #{more_files}"
-        else
-          # This version is a bit slower, but not too much, and it's
-          # not sensitive to vagaries of various 'tail' commands.
-          tails = parts.drop(1).map { |path| "tail +2 #{path}" }
-          more = tails.join(';')
-        end
-        command = "(cat #{parts[0]}; #{more}) >#{temp}"
-        STDERR.puts(command)
-        system command
-        FileUtils.mv temp, path
-        STDERR.puts("#{File.basename(path)}: #{skip} records")
-        path
-      end
+      parts, count = obtain_parts(query, columns, path)
+      assemble_parts(parts, count, path)
     end
   end
 
-  def query(cql)
+  # Ensure that all the parts files for a table exist, using Neo4j to
+  # obtain them as needed.
+  # Returns a list of paths (file names for the parts) and a count of
+  # the total number of records.
+
+  def obtain_parts(query, columns, path)
+
+    # Create a directory path.parts to hold the parts
+    parts_dir = path + ".parts"
+
+    if @chunksize
+      limit = "#{@chunksize}"
+    else
+      limit = "10000000"
+    end
+
+    parts = []
+    skip = 0
+    while true
+      # Fetch it in parts
+      part = File.join(parts_dir, "#{skip}.csv")
+      if File.exist?(part)
+        STDERR.puts "reusing previously created #{part}"
+        parts.push(part)
+        # TBD: we should increase skip by the actual number of
+        # records in the file.
+        skip += @chunksize if @chunksize
+      else
+        result = run_query(query + " SKIP #{skip} LIMIT #{limit}")
+        # A null result means that there was some kind of error, which
+        # has already been reported.  (because I don't want to learn
+        # ruby exception handling!)
+        got = result["data"].length
+        if result and got > 0
+          emit_csv(result, columns, part)
+          parts.push(part)
+        end
+        skip += got
+        break if @chunksize and got < @chunksize
+      end
+      break unless @chunksize
+    end
+    [parts, skip]
+  end
+
+  # Combine the parts files (for a single table) into a single master .csv file
+
+  def assemble_parts(parts, count, path)
+    # Concatenate all the parts together
+    if parts.size == 0
+      nil
+    elsif parts.size == 1
+      FileUtils.mv parts[0], path
+      path
+    else
+      temp = path + ".new"
+      tails = parts.drop(1).map { |path| "tail +2 #{path}" }
+      more = tails.join(';')
+      command = "(cat #{parts[0]}; #{more}) >#{temp}"
+      system command
+      FileUtils.mv temp, path
+      STDERR.puts("#{File.basename(path)}: #{parts.length} parts, #{count} records")
+      path
+    end
+  end
+
+  # Run a single CQL query using method provided
+
+  def run_query(cql)
     @query_fn.call(cql)
   end
+
+  # Method for doing queries using EOL v3 API via HTTP
 
   def self.query_via_http(server, token, cql)
     # Need to be a web client.
@@ -276,7 +297,7 @@ class TraitsDumper
     end
   end
 
-  # Utility
+  # Utility - convert native cypher output form to CSV
   def emit_csv(start, keys, path)
 
     # Sanity check the result
