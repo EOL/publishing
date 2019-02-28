495706151c6a312ebf0adb1bc66ad9e004b882e9
JRice <jrice@eol.org>
2019-02-28 16:36:19 -0500

    PageContent.fix_all_duplicate_positions

--- app/models/page_content.rb
+++ app/models/page_content.rb
@@ -38,95 +38,145 @@ class PageContent < ActiveRecord::Base
   # them separately, which I think has advantages)
   acts_as_list scope: :page
 
-  # content_id: 9368757, page_id: 8663597
-  def self.set_v2_exemplars(starting_page_id = nil)
-    puts "[#{Time.now}] starting"
-    STDOUT.flush
-    require 'csv'
-    # Jamming this in the /public/data dir just so we can keep it between restarts!
-    file = Rails.root.join('public', 'data', 'image_order.tsv')
-    all_data = CSV.read(file, col_sep: "\t")
-    per_cent = all_data.size / 100
-    all_data[1..-1].each_with_index do |row, i|
-      medium_id = row[0]
-      page_id = row[1]
-      next if starting_page_id && page_id.to_i < starting_page_id.to_i
-      order = row[2].to_i # 0-index
-      last = (row[3] =~ /last/i) # 'first' or 'last'
-      contents = PageContent.where(content_type: 'Medium', content_id: medium_id, page_id: page_id)
-      if contents.any?
-        content = contents.first # NOTE: #shift does not work on ActiveRecord_Relation, sadly.
-        if contents.size > 1
-          contents[1..-1].each { |extra| extra.destroy } # Remove duplicates
+  class << self
+    # content_id: 9368757, page_id: 8663597
+    def set_v2_exemplars(starting_page_id = nil)
+      puts "[#{Time.now}] starting"
+      STDOUT.flush
+      require 'csv'
+      # Jamming this in the /public/data dir just so we can keep it between restarts!
+      file = Rails.root.join('public', 'data', 'image_order.tsv')
+      all_data = CSV.read(file, col_sep: "\t")
+      per_cent = all_data.size / 100
+      fixed_page = 0
+      all_data[1..-1].each_with_index do |row, i|
+        medium_id = row[0]
+        page_id = row[1]
+        next if starting_page_id && page_id.to_i < starting_page_id.to_i
+        if fixed_page < page_id
+          fixed_page = page_id
+          fix_duplicate_positions(page_id)
         end
-        if last
-          content.move_to_bottom # Let's not worry about the ORDER of the worst ones; I think it will naturally work.
-        else
-          if order.zero?
-            PageIcon.create(page_id: page_id, medium_id: medium_id, user_id: 1)
-            content.move_to_top
-          else
-            content.insert_at(order + 1)
+        order = row[2].to_i # 0-index
+        last = (row[3] =~ /last/i) # 'first' or 'last'
+        contents = PageContent.where(content_type: 'Medium', content_id: medium_id, page_id: page_id)
+        if contents.any?
+          content = contents.first # NOTE: #shift does not work on ActiveRecord_Relation, sadly.
+          if contents.size > 1
+            contents[1..-1].each { |extra| extra.destroy } # Remove duplicates
           end
-        end
-        if (i % per_cent).zero?
-          puts "[#{Time.now}] ... #{i / per_cent}"
+          if last
+            content.move_to_bottom # Let's not worry about the ORDER of the worst ones; I think it will naturally work.
+          else
+            if order.zero?
+              PageIcon.create(page_id: page_id, medium_id: medium_id, user_id: 1)
+              content.move_to_top
+            else
+              content.insert_at(order + 1)
+            end
+          end
+          if (i % per_cent).zero?
+            puts "[#{Time.now}] ... #{i / per_cent}"
+            STDOUT.flush
+          end
+        else
+          puts "[#{Time.now}] missing: content_type: 'Medium', content_id: #{medium_id}, page_id: #{page_id}"
           STDOUT.flush
         end
-      else
-        puts "[#{Time.now}] missing: content_type: 'Medium', content_id: #{medium_id}, page_id: #{page_id}"
-        STDOUT.flush
       end
+      puts "[#{Time.now}] done."
+      STDOUT.flush
     end
-    puts "[#{Time.now}] done."
-    STDOUT.flush
-  end
 
-  def self.fix_exemplars
-    # NOTE: this does NOT restrict by content_type because that slows the query WAAAAAAY down (it's not indexed)
-    page_ids = uniq.pluck(:page_id)
-    batches = (page_ids.size / 1000).ceil
-    puts "++ Cleaning up #{page_ids.size} exemplars (#{batches} batches)..."
-    batch = 1
-    page_ids.in_groups_of(1000, false) do |group|
-      puts "++ Batch #{batch}/#{batches}..."
-      # NOTE: The #search_import is required because we're going to update Search... without that scope added, we end up
-      # doing dozens of extra DB queries to build the Search JSON!
-      Page.search_import.where(id: group).find_each do |page|
-        # NOTE: yes, this will produce a query for EVERY page in the array. ...But it's very hard to limit the number of results from a join, and this isn't a method we'll run very often, so this is "Fine."
-        medium_id = page.media.order(:position).limit(1).pluck(:id).first
-        page.update_attribute(:medium_id, medium_id) unless page.medium_id == medium_id
-      end
-      batch += 1
-    end
-    puts "++ Done."
-  end
-
-  def self.export_for_ordering
-    require 'csv'
-    collection_num = 1
-    collection = []
-    puts "start #{Time.now}"
-    # NOTE: YES! Really, one at a time was *fastest*. Go. Figure.
-    Page.select('id').find_each do |page|
-      where(page_id: page.id).visible.not_untrusted.media.includes(:content).find_each do |item|
-        collection << [item.content_id, item.page_id, item.content.source_url, item.position]
-        if collection.size >= 10_000
-          flush_collection(collection, collection_num)
-          collection = []
-          collection_num += 1
-          puts "flushed ##{collection_num - 1} @ #{Time.now}"
+    # NOTE: a lot of this is duplicated above. It's temporary code, I didn't think it was worth extracting it. :|
+    def fix_all_duplicate_positions
+      puts "[#{Time.now}] starting"
+      STDOUT.flush
+      last_flush = Time.now
+      require 'csv'
+      # Jamming this in the /public/data dir just so we can keep it between restarts!
+      file = Rails.root.join('public', 'data', 'image_order.tsv')
+      all_data = CSV.read(file, col_sep: "\t")
+      per_cent = all_data.size / 100
+      fixed_page = 0
+      all_data[1..-1].each_with_index do |row, i|
+        medium_id = row[0].to_i
+        page_id = row[1].to_i
+        if (i % per_cent).zero? || last_flush < 5.minutes.ago
+          puts "[#{Time.now}] % COMPLETE: #{i / per_cent}"
+          STDOUT.flush
+          last_flush = Time.now
+        end
+        if fixed_page < page_id
+          fixed_page = page_id
+          fix_duplicate_positions(page_id)
+        else
+          next
         end
       end
     end
-    puts "end #{Time.now}"
-    flush_collection(collection, collection_num) unless collection.empty?
-  end
 
-  def self.flush_collection(collection, collection_num)
-    CSV.open(Rails.root.join('public', "images_for_sorting_#{collection_num}.csv"), 'wb') do |csv|
-      csv << %w[eol_pk page_id source_url position]
-      collection.each { |row| csv << row }
+    def fix_duplicate_positions(page_id)
+      return if PageContent.where(page_id: page_id, position: 0).count <= 1 ||
+                PageContent.where(page_id: page_id, position: 1).count <= 1
+      exemplar = Page.find(page_id).page_icon.medium_id
+      final = PageContent.where(page_id: page_id).maximum(:position) + 1
+      [0,1].each do |position|
+        PageContent.where(page_id: page_id, position: position).find_each do |content|
+          if exemplar != content.content_id # Don't move the exemplar.
+            content.update_attribute(:position, final)
+            final += 1
+          end
+        end
+      end
     end
-  end
+
+    def fix_exemplars
+      # NOTE: this does NOT restrict by content_type because that slows the query WAAAAAAY down (it's not indexed)
+      page_ids = uniq.pluck(:page_id)
+      batches = (page_ids.size / 1000).ceil
+      puts "++ Cleaning up #{page_ids.size} exemplars (#{batches} batches)..."
+      batch = 1
+      page_ids.in_groups_of(1000, false) do |group|
+        puts "++ Batch #{batch}/#{batches}..."
+        # NOTE: The #search_import is required because we're going to update Search... without that scope added, we end up
+        # doing dozens of extra DB queries to build the Search JSON!
+        Page.search_import.where(id: group).find_each do |page|
+          # NOTE: yes, this will produce a query for EVERY page in the array. ...But it's very hard to limit the number of results from a join, and this isn't a method we'll run very often, so this is "Fine."
+          medium_id = page.media.order(:position).limit(1).pluck(:id).first
+          page.update_attribute(:medium_id, medium_id) unless page.medium_id == medium_id
+        end
+        batch += 1
+      end
+      puts "++ Done."
+    end
+
+    def export_for_ordering
+      require 'csv'
+      collection_num = 1
+      collection = []
+      puts "start #{Time.now}"
+      # NOTE: YES! Really, one at a time was *fastest*. Go. Figure.
+      Page.select('id').find_each do |page|
+        where(page_id: page.id).visible.not_untrusted.media.includes(:content).find_each do |item|
+          collection << [item.content_id, item.page_id, item.content.source_url, item.position]
+          if collection.size >= 10_000
+            flush_collection(collection, collection_num)
+            collection = []
+            collection_num += 1
+            puts "flushed ##{collection_num - 1} @ #{Time.now}"
+          end
+        end
+      end
+      puts "end #{Time.now}"
+      flush_collection(collection, collection_num) unless collection.empty?
+    end
+
+    def flush_collection(collection, collection_num)
+      CSV.open(Rails.root.join('public', "images_for_sorting_#{collection_num}.csv"), 'wb') do |csv|
+        csv << %w[eol_pk page_id source_url position]
+        collection.each { |row| csv << row }
+      end
+    end
+  end # of class methods
 end
