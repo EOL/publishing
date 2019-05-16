c6828baae60aff246dbe64276db7153a992386ce
JRice <jrice@eol.org>
2019-05-16 16:39:25 -0400

    Ooops, found bug with logging page id in Page::Reindexer, also bug with batch size argument

--- app/models/page/reindexer.rb
+++ app/models/page/reindexer.rb
@@ -17,29 +17,37 @@ class Page::Reindexer
     log("Skipping to page #{@start_page_id}") unless @start_page_id == 1
     current_page_id = @start_page_id
     begin
-      Page.search_import.where(['id >= ?', @start_page_id]).find_in_batches(batch_size: 10) do |pages|
+      count = Page.search_import.where(['id >= ?', @start_page_id]).count
+      log("Total pages to index: #{count}")
+      batches = count / @batch_size + 1
+      batch = 0
+      Page.search_import.where(['id >= ?', @start_page_id]).find_in_batches(batch_size: @batch_size) do |pages|
+        batch += 1
         current_page_id = pages.first.id
         begin
           Page.search_index.bulk_update(pages, :search_data)
         rescue Searchkick::ImportError
-          log('An update failed, trying index')
+          log("An update failed (starting with page #{current_page_id}), trying index")
           Page.search_index.bulk_index(pages)
         rescue Faraday::ConnectionFailed
-          log('Connection failed. You may want to check any other scripts that were running. Retrying...')
+          log("Connection failed (starting with page #{current_page_id}), you may want to check any other scripts that were running. Retrying...")
           sleep(6) if @throttle
           pages.each do |page|
             begin
               current_page_id = page.id
               page.reindex
-              sleep(2) if @throttle # Ouch. Sleeping per page is ugly!
+              sleep(1) if @throttle # Ouch. Sleeping per page is ugly!
             rescue => e
               log("Indexing page #{page.id} FAILED. Skipping. Error: #{e.message}")
             end
           end
         end
+        sleep(1) if @throttle
+        if (batch % 10) == 1
+          pct = (batch.to_f / batches * 1000).ceil / 10.0
+          log("#{pages.last.id} (batch #{batch}/#{batches}, #{pct}%)")
+        end
       end
-      sleep(2) if @throttle
-      log(pages.last.id) 
     rescue => e
       log("DIED: restart with ID #{current_page_id}")
       raise(e)
