2b04852918f5110ed44651b21e5575c94b9807b2
Jonathan A Rees <jar398@mumble.net>
2019-07-15 14:10:22 -0400

    weaken capabilities of admin users

--- app/controllers/service/cypher_controller.rb
+++ app/controllers/service/cypher_controller.rb
@@ -8,38 +8,50 @@ class Service::CypherController < ServicesController
   def form
     # Cf. the view
   end
-
+  
   def query
-    cypher = params[:query]
-    return render_bad_request(title: "Missing a 'query' parameter.") \
-      unless cypher != nil
+    format = params.delete(:format) || "cypher"
 
-    format = params[:format]
-    format = "cypher" unless format != nil
+    cypher = params.delete(:query])
+    return render_bad_request(title: "Missing a 'query' parameter.") unless
+      cypher != nil
 
-    # Authenticate the user and authorize the requested operation, using
-    # the API token and the user's information in the table.
-    # Non-admin users are not supposed to modify the database.  The following
-    # regex is an ad hoc filter to try to prevent this, at least when it is 
-    # accidental; one shouldn't expect it to be secure against a concerted attack.
+    # Deletion is forbidden in order to help prevent catastrophic
+    # mistakes.
     # The purpose of the 'limit' is to help prevent unintended DoS 
     # attacks.
-    if cypher =~ /\b(delete|create|set|remove|merge|call|drop|load)\b/i
+    # The regexes below are ad hoc filters to try to prevent problems,
+    # at least when accidental.  We don't expect these permission
+    # checks to provide security against a concerted attack.
+
+    return render_bad_request(title: "Please provide a LIMIT clause") unless
+      cypher =~ /\b(limit)\b/i
+    return render_bad_request(title: "Cypher operation not permitted via service") if
+      cypher =~ /\b(delete|remove|call)\b/i
+      
+    # Authenticate the user and authorize the requested operation, 
+    # using the API token and the information in the user table.
+    # Non-admin users are not supposed to add to the database.
+    
+    if cypher =~ /\b(create|set|merge|load)\b/i
+      # Allow admin to add to graph
       user = authorize_admin_from_token!
-    elsif cypher =~ /\b(limit)\b/i
-      user = authorize_user_from_token!    # power user that is
     elsif
-      user = authorize_admin_from_token!
+      # Power users can only read
+      user = authorize_user_from_token!
     end
     return nil unless user
 
     # Do the query or command
+    render_results(TraitBank.query(cypher), format)
+  end
+
+  def render_results(results, format)
     case format
     when "cypher" then
-      render json: TraitBank.query(cypher)
+      render json: results
     when "csv" then
       self.content_type = 'text/csv'
-      results = TraitBank.query(cypher)
       # Streaming output
       self.response_body =
         Enumerator.new do |y|
@@ -52,6 +64,25 @@ class Service::CypherController < ServicesController
       return render_bad_request(title: "Unrecognized 'format' parameter value.", format: format)
     end        # end case
   end          # end def
+
+  def remove_relationships
+    format = params[:format] || "cypher"
+
+    relation = Integer(params[:relation])
+    # Fixed set of allowed relationships
+    return render_bad_request(title: "Unrecognized relation #{relation}") unless
+      ["inferred_trait"].include?(relation)
+
+    resource_id = Integer(params[:resource_id])
+    authorize_admin_from_token!
+    render_results(TraitBank.query(
+                    'MATCH ()-[rel:#{relation}]-
+                           (t:Trait)-[:supplier]->
+                           (:Resource {resource_id: #{resource_id}})
+                     DELETE rel
+                     RETURN t.resource_pk'))
+  end
+
 end            # end class
 
 
