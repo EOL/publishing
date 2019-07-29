dc487482c9a13070680551397c1bb38977c253b3
JRice <jrice@eol.org>
2019-07-29 13:26:44 -0400

    Net::HTTP.new uses different syntax than Net::HTTP.start

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -4,6 +4,7 @@ class Repository
     repo_url = Rails.application.secrets.repository['url']
     @repo_site = URI(repo_url)
     @log = log # Okay if it's nil.
+    @unacceptable_codes = 300
   end
 
   def is_on_this_host?
@@ -16,9 +17,10 @@ class Repository
 
   def exists?(name)
     url = URI.parse(file_url(name))
-    req = Net::HTTP.new(@repo_site.host, @repo_site.port, use_ssl: @repo_site.scheme == 'https')
+    req = Net::HTTP.new(@repo_site.host, @repo_site.port)
+    req.use_ssl = @repo_site.scheme == 'https'
     res = req.request_head(url.path)
-    res.code.to_i < 400
+    res.code.to_i < @unacceptable_codes
   end
 
   def copy_file(local_name, remote_name)
@@ -31,7 +33,7 @@ class Repository
     result = Net::HTTP.start(@repo_site.host, @repo_site.port, use_ssl: @repo_site.scheme == 'https') do |http|
       resp = http.get(url)
     end
-    unless result.code.to_i < 400
+    unless result.code.to_i < @unacceptable_codes
       log_warn("MISSING #{@repo_site}#{url} [#{result.code}] (#{resp.size} bytes); skipping")
       return false
     end
