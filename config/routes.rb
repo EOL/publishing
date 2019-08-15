c8f85e35cd36d2b0679f862ee538f94e923106b7
Michael Vitale <mmvitale@gmail.com>
2019-08-15 09:47:36 -0400

    remove wordcloud test route

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
     get 'errors/not_found'
     get 'errors/internal_server_error'
 
-    get 'wordcloud_test' => "pages#wordcloud_test"
-    
     # This line mounts Refinery's routes at the root of your application.
     # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
     # If you would like to change where this extension is mounted, simply change the
