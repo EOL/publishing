f336f97dedcf53a8cd88cb83bd3ddc24dc8b28c0
Michael Vitale <mmvitale@gmail.com>
2019-09-02 15:18:11 -0400

    use scientific names in curated hierarchies

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -82,7 +82,7 @@ module PagesHelper
 
   def classification_content(page, this_node, node, ancestors)
     haml_tag("div.item") do
-      summarize(page, name: node.name, current_page: node == this_node, node: node, no_icon: true)
+      summarize(page, name: node.scientific_name, current_page: node == this_node, node: node, no_icon: true)
       if ancestors.empty?
         if this_node.children.any?
           haml_tag("div.item") do
@@ -90,7 +90,7 @@ module PagesHelper
               # sanitize so <i> tags aren't counted for sorting purposes
               this_node.children.sort { |a, b| sanitize(a.name, tags: []) <=> sanitize(b.name, tags: []) }.each do |child|
                 haml_tag("div.item") do
-                  summarize(child.page, name: child.name, node: child, no_icon: true)
+                  summarize(child.page, name: child.scientific_name, node: child, no_icon: true)
                 end
               end
             end
@@ -106,7 +106,7 @@ module PagesHelper
     if ancestors.empty?
       this_node.siblings.each do |sibling|
         haml_tag("div.item") do
-          summarize(sibling.page, name: sibling.name, current_page: false, node: sibling, no_icon: true)
+          summarize(sibling.page, name: sibling.scientific_name, current_page: false, node: sibling, no_icon: true)
         end
       end 
     end
