ee8c344846f884dbe9a53f9b13a6f2f1d039b219
JRice <jrice@eol.org>
2019-09-20 12:03:54 -0400

    Change map path.

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -2,7 +2,7 @@ module MapsHelper
   def map_json_path(page)
     if page.occurrence_map?
       prefix = page.id.to_i % 100
-      asset_path("/data/maps/#{prefix}/#{page.id}.json")
+      asset_path("/data/map_data_dwca/#{prefix}/#{page.id}.json")
     else
       nil
     end
