da935e847850af78400fd2848686263f4d9c4373
Michael Vitale <mmvitale@gmail.com>
2019-09-22 16:08:04 -0400

    order page articles by resource name, article name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -482,6 +482,7 @@ private
 
     @articles = articles_with_lang_group if @lang_group != ALL_LANG_GROUP
     @articles = @articles.where({ resource_id: resource_id }) if !resource_id.nil?
+    @articles = @articles.order("resources.name asc, articles.name asc")
     @all_lang_group = ALL_LANG_GROUP
   end
 
