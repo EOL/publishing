2db5381ff46188564f798e999a0f984bbef2bdd2
JRice <jrice@eol.org>
2019-11-12 14:48:24 -0500

    Trying a different way to read large batches of download page info

--- app/models/trait_bank/page_download_writer.rb
+++ app/models/trait_bank/page_download_writer.rb
@@ -1,17 +1,12 @@
 module TraitBank::PageDownloadWriter
   def self.to_arrays(hashes, url)
-    all_pages = Page.where(:id => TraitBank::DownloadUtils.page_ids(hashes)).
-      includes(:preferred_scientific_names, :preferred_vernaculars)
     data = []
-    all_pages.find_in_batches do |pages|
+    TraitBank::DownloadUtils.page_ids(hashes).in_groups_of(10_000) do |page_ids|
+      pages = Page.where(:id => page_ids).includes(:preferred_scientific_names, :preferred_vernaculars)
       data << (cols.keys << url)
       hashes.each do |result|
         page_id = TraitBank::DownloadUtils.page_id(result)
         page = pages.find { |p| p.id == page_id }
-        # NOTE: this is not terribly efficient. :\ But because we're pulling the ID out using that
-        # DownloadUtils.page_id, it's tricky (but not impossible... just ... ugly code) to group the hashes first. Note
-        # that if we're in batches and this batch doesn't have your page, this w/b common. Luckily, this is a
-        # *background* task, so should be ... okay.
         next if !page
         sci_name = page.preferred_scientific_names.first
         row = []
