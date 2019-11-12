ebca08a225d327377ff0cd8175802b52565d9486
JRice <jrice@eol.org>
2019-11-12 14:20:11 -0500

    Process b/g PageDownloadWriter data downloads in batches, if they are quite large.

--- app/models/trait_bank/page_download_writer.rb
+++ app/models/trait_bank/page_download_writer.rb
@@ -1,21 +1,26 @@
 module TraitBank::PageDownloadWriter
   def self.to_arrays(hashes, url)
-    pages = Page.where(:id => TraitBank::DownloadUtils.page_ids(hashes)).
+    all_pages = Page.where(:id => TraitBank::DownloadUtils.page_ids(hashes)).
       includes(:preferred_scientific_names, :preferred_vernaculars)
     data = []
-    data << (cols.keys << url)
-    hashes.each do |result|
-      page_id = TraitBank::DownloadUtils.page_id(result)
-      page = pages.find { |p| p.id == page_id }
-      next if !page
-      sci_name = page.preferred_scientific_names.first
-      row = []
-      self.cols.each do |_, lamb|
-        row << lamb[page, sci_name]
+    all_pages.find_in_batches do |pages|
+      data << (cols.keys << url)
+      hashes.each do |result|
+        page_id = TraitBank::DownloadUtils.page_id(result)
+        page = pages.find { |p| p.id == page_id }
+        # NOTE: this is not terribly efficient. :\ But because we're pulling the ID out using that
+        # DownloadUtils.page_id, it's tricky (but not impossible... just ... ugly code) to group the hashes first. Note
+        # that if we're in batches and this batch doesn't have your page, this w/b common. Luckily, this is a
+        # *background* task, so should be ... okay.
+        next if !page
+        sci_name = page.preferred_scientific_names.first
+        row = []
+        self.cols.each do |_, lamb|
+          row << lamb[page, sci_name]
+        end
+        data << row
       end
-      data << row
     end
-
     data
   end
 
