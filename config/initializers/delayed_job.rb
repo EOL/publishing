82c6655365d6e7aa098ac91a6e4953c94b8a2d4d
JRice <jrice@eol.org>
2019-11-14 12:12:59 -0500

    Extracting a UserDownload method for easier mantaineance and lowering the time jobs are allowed to run there

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,6 +1,7 @@
-# Really this max run time is ridiculous, but I don't ever want to hit it. We do real work and we watch the long-running
-# jobs.
-Delayed::Worker.max_run_time = 14.days
+# Nov 2019: we decided to lower this from the nigh-unlimited "2 weeks" that it was before, because we had a job running
+# for two days (before we killed it) that was gumming up the works. We thought "overnight" was rasonable, and this just
+# about hits that, we think:
+Delayed::Worker.max_run_time = 10.hours
 Delayed::Worker.logger = Logger.new(Rails.root.join('log', 'delayed_job.log'))
 # It's possible that something went wrong with Delayed::Job, but, really, we don't *usually* want to re-try jobs. :\
 Delayed::Worker.max_attempts = 2
