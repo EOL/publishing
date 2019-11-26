e5a490dfc759d5eaf2574eec1700a52fe2c64590
JRice <jrice@eol.org>
2019-11-26 09:24:54 -0500

    Now with more migrations actually run!

--- db/migrate/20191125215809_add_media_subtype_to_page_contents.rb
+++ db/migrate/20191125215809_add_media_subtype_to_page_contents.rb
@@ -3,7 +3,7 @@ class AddMediaSubtypeToPageContents < ActiveRecord::Migration
     add_column :page_contents, :content_subclass, :integer, default: 0
     PageContent.connection.execute(
       'UPDATE page_contents pc LEFT JOIN media m ON pc.content_id = m.id '\
-        'c nvbc  SET pc.content_subclass = m.subclass WHERE pc.content_type = "Medium"'
+        'SET pc.content_subclass = m.subclass WHERE pc.content_type = "Medium"'
     )
     # YOU WERE HERE: This is all fine and dandy, but you'll also have to update the harvester to set and import this
     # column, and make sure that any content-inserting code set this value where appropriate (I think harvesting may be
