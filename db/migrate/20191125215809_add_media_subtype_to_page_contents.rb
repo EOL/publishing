081dfe39ce78c4c6e06b496ef612582ab5f04feb
JRice <jrice@eol.org>
2019-12-04 12:37:26 -0500

    Updating the most recent migration, 20191125215809

--- db/migrate/20191125215809_add_media_subtype_to_page_contents.rb
+++ db/migrate/20191125215809_add_media_subtype_to_page_contents.rb
@@ -1,12 +1,21 @@
+# 20191125215809
 class AddMediaSubtypeToPageContents < ActiveRecord::Migration
-  def change
-    add_column :page_contents, :content_subclass, :integer, default: 0
-    PageContent.connection.execute(
-      'UPDATE page_contents pc LEFT JOIN media m ON pc.content_id = m.id '\
-        'SET pc.content_subclass = m.subclass WHERE pc.content_type = "Medium"'
-    )
+  def up
+    default = Medium.subclasses[:image] # Probably 0, but let's be safe.
+    add_column :page_contents, :content_subclass, :integer, default: default
+    Medium.subclasses.values.each do |subclass|
+      next if subclass == default
+      Medium.where(subclass: subclass).select(['id']).find_in_batches do |batch|
+        ids = batch.map(&:id)
+        PageContent.where(content_type: 'Medium', content_id: ids).update_all(content_subclass: subclass)
+      end
+    end
     # YOU WERE HERE: This is all fine and dandy, but you'll also have to update the harvester to set and import this
     # column, and make sure that any content-inserting code set this value where appropriate (I think harvesting may be
     # the only case, but check).
+  end
+
+  def down
+    remove_column :page_contents, :content_subclass
   end
 end
