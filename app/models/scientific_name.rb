8bb6d6c3f213d61f7d02788e016c33f5278e924d
Michael Vitale <mmvitale@gmail.com>
2019-12-06 15:48:55 -0500

    stopgap names tab fix -- add 'valid' to list of preferred taxonomic statuses

--- app/models/scientific_name.rb
+++ app/models/scientific_name.rb
@@ -10,7 +10,7 @@ class ScientificName < ActiveRecord::Base
 
   counter_culture :page
 
-  DISPLAY_STATUS_PREFERRED_VALUES = Set.new(["accepted", "preferred"])
+  DISPLAY_STATUS_PREFERRED_VALUES = Set.new(["accepted", "preferred", "valid"])
 
   # We discovered that about 40 resources were affected by a bug where the #scientific_name attribute of a Node could be
   # assigned to a non-preferred ScientificName. This code detects those problems and heals them.
