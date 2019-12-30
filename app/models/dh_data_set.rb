0c6a5d88c83f39e7b87eabea2573ed2a76f39dcd
Michael Vitale <mmvitale@gmail.com>
2019-12-30 11:14:09 -0500

    move DhDataSet seed method to that class

--- app/models/dh_data_set.rb
+++ app/models/dh_data_set.rb
@@ -1,4 +1,25 @@
+require "csv"
+
 class DhDataSet < ActiveRecord::Base
   validates_presence_of :dataset_id
   validates_presence_of :name
+
+  TSV_PATH = Rails.application.root.join("db", "seed_data", "dh_data_sets.tsv")
+
+  class << self
+    def rebuild_from_tsv
+      model_data = []
+      CSV.foreach(TSV_PATH, col_sep: "\t", headers: true) do |row|
+        model_data << {
+          dataset_id: row["datasetID"],
+          name: row["name"]
+        }
+      end
+
+      self.transaction do
+        self.destroy_all
+        self.create(model_data)
+      end
+    end
+  end
 end
