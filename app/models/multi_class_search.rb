6083201d3ef7654ead9ea044ea0f487023569e6b
Michael Vitale <mmvitale@gmail.com>
2020-01-08 12:21:51 -0500

    never use 'phrase' match for page search

--- app/models/multi_class_search.rb
+++ app/models/multi_class_search.rb
@@ -151,11 +151,10 @@ class MultiClassSearch
   def prepare_pages_query
     if @types[:pages]
       fields = %w[preferred_vernacular_strings^20 vernacular_strings^20 preferred_scientific_names^10 scientific_name^10 synonyms^10 providers resource_pks]
-      match = @words.size == 1 ? :text_start : :phrase
       basic_search(
         Page,
         #boost_by: [:page_richness, :specificity, :depth],
-        match: match,
+        match: :text_start,
         fields: fields,
         where: @clade ? { ancestry_ids: @clade.id } : nil,
         includes: [:preferred_vernaculars, :medium, { native_node: { node_ancestors: :ancestor } }]
