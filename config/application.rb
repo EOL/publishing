7d5618347d828a9b016a5a6a0e221b54e344cf78
Michael Vitale <mmvitale@gmail.com>
2020-03-25 13:09:36 -0400

    fix zh-TW locale

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module EolWebsite
     config.i18n.default_locale = :en
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     # you also have to modify config/initalizers/refinery/i18n.rb if you want to enable a new locale :(
-    config.i18n.available_locales = %i(en mk fi pt-BR fr :zh-TW pms tr)
+    config.i18n.available_locales = %i(en mk fi pt-BR fr zh-TW pms tr)
 
     config.exceptions_app = self.routes
     config.data_glossary_page_size = 250
