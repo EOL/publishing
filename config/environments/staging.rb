ab4807553deb1d83dc36f16a09f6cf63d7670679
Michael Vitale <mmvitale@gmail.com>
2020-03-25 13:04:23 -0400

    fix i18n fallback config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = :en
+  config.i18n.fallbacks = true
 
   config.after_initialize do
     ActiveRecord::Base.logger = Rails.logger.clone
