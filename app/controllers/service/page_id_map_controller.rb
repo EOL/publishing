280d9ef025ad216d39ed483cfb8eb35af5b80057
Jonathan A. Rees <jar398@mumble.net>
2020-04-02 08:50:51 -0400

    fix comments

--- app/controllers/service/page_id_map_controller.rb
+++ app/controllers/service/page_id_map_controller.rb
@@ -5,24 +5,24 @@
 # matcher.) This gives a toehold for comparing graphdb contents with
 # opendata resources, and for extending the graphdb with information in
 # opendata resources. The immediate need for this service was to get
-# vernaculars into the graphdb, but I expect it to have other uses,
+# vernaculars into the graphdb, but I [JAR] expect it to have other uses,
 # e.g. I may rewrite the ranks 'script' to use this service instead of
-# the provider_ids.csv file.
+# the provider_ids.csv file to obtain the mapping between DH node ids
+# and page ids.
 
 # Parameters are resource (id), limit, skip. The default limit is 100000.
 
 # Testing has been minimal, just manual tests using wget e.g.
 # wget -O - "http://localhost:3000/service/page_id_map?resource=40&limit=5"
 
-# Does the http client have to be authenticated (using a token)? I don't
-# see this as important, but it's easy enough to un-comment the
-# lines that handle this.
+# Does the http client have to be authorized (using an API token)? I
+# don't see this as important, but it's easy enough to un-comment the
+# line that does the authorization check.
 
 require 'csv'
 
 class Service::PageIdMapController < ServicesController
-  #before_action :require_power_user, only: :form
-  #skip_before_action :verify_authenticity_token, only: :command
+  #before_action :require_power_user
 
   def get
     self.content_type = 'text/csv'
