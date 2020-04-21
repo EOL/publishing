b06f8d02a0ab9b34bce1600816d9da936b782051
JRice <jrice@eol.org>
2020-04-21 18:34:45 -0400

    Trying a new approach

--- app/models/trait_bank/logger.rb
+++ app/models/trait_bank/logger.rb
@@ -1,15 +1,12 @@
 class TraitBank
   class Logger
     class << self
-      def logger
-        @logger ||= ActiveSupport::TaggedLogging.new(Logger.new(Rails.root.join('log', 'traitbank.log')))
-      end
-
       def log(message, tag = nil)
         tag ||= 'INFO'
         time = Time.now
         delta = @last_time.nil? ? '' : "<#{(time - @last_time).round(3)}s>"
-        logger.tagged(tag) { @logger.warn("[#{time.strftime('%F %T')}]#{delta} #{message}") }
+        @logger ||= ActiveSupport::TaggedLogging.new(Logger.new(Rails.root.join('log', 'traitbank.log')))
+        @logger.tagged(tag) { @logger.warn("[#{time.strftime('%F %T')}]#{delta} #{message}") }
         @last_time = time
       end
 
