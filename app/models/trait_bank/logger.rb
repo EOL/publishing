1819c1ed1aa844689fbff519a4d8ba6bd9d097bf
JRice <jrice@eol.org>
2020-04-21 18:38:50 -0400

    Ahh! Context. It is trying to call itself.

--- app/models/trait_bank/logger.rb
+++ app/models/trait_bank/logger.rb
@@ -5,7 +5,7 @@ class TraitBank
         tag ||= 'INFO'
         time = Time.now
         delta = @last_time.nil? ? '' : "<#{(time - @last_time).round(3)}s>"
-        @logger ||= ActiveSupport::TaggedLogging.new(Logger.new(Rails.root.join('log', 'traitbank.log')))
+        @logger ||= ActiveSupport::TaggedLogging.new(::Logger.new(Rails.root.join('log', 'traitbank.log')))
         @logger.tagged(tag) { @logger.warn("[#{time.strftime('%F %T')}]#{delta} #{message}") }
         @last_time = time
       end
