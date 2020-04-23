0cb3311149769c16dd27138a4b331b24c5b05248
JRice <jrice@eol.org>
2020-04-23 09:26:08 -0400

    Language::Loader.parse and .read_file for adding languages.

--- db/migrate/20200102161828_add_languages.rb
+++ db/migrate/20200102161828_add_languages.rb
@@ -1,15 +1,5 @@
 class AddLanguages < ActiveRecord::Migration[4.2]
   def change
-    require 'csv'
-    CSV.foreach(Rails.root.join('doc', 'new_languages.csv')) do |line|
-      next if line[2].blank? # No language name, have to skip it.
-      if Language.where(code: line[0]).exists?
-        lang = Language.find_by_code(line[0])
-        lang.group = line[1]
-        lang.save!
-      else
-        Language.create(code: line[0], group: line[1], can_browse_site: false)
-      end
-    end
+    Language::Loader.load_file(Rails.root.join('doc', 'new_languages.csv'))
   end
 end
