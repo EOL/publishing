b7bfccb7f24fc401cf70ea34d970f40a0694db58
Michael Vitale <mmvitale@gmail.com>
2020-05-08 14:19:43 -0400

    show all locale options on editor pages interface etc

--- app/models/editor_page.rb
+++ app/models/editor_page.rb
@@ -1,4 +1,11 @@
 class EditorPage < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
+
+  validates :name, presence: true, uniqueness: true
+  has_many :editor_page_translations
+
+  def translation_for_locale(locale)
+    editor_page_translations.where(locale: locale)&.first
+  end
 end
