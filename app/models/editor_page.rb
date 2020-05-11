308ce16fdfcdb1d2bd7ef77702417006c7154c71
Michael Vitale <mmvitale@gmail.com>
2020-05-11 15:10:36 -0400

    draft publishing

--- app/models/editor_page.rb
+++ app/models/editor_page.rb
@@ -12,4 +12,13 @@ class EditorPage < ApplicationRecord
   def published_for_locale(locale)
     editor_page_contents.where(locale: locale, status: :published)&.first
   end
+
+  # These raise a not found error, like find
+  def find_draft_for_locale(locale)
+    editor_page_contents.find_by(locale: locale, status: :draft)
+  end
+
+  def find_published_for_locale(locale)
+    editor_page_contents.find_by(locale: locale, status: :published)
+  end
 end
