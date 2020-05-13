3936e8c3148e595f86a0332562e69d6bbce5e74f
Michael Vitale <mmvitale@gmail.com>
2020-05-13 13:40:30 -0400

    placeholder for image attachment handling

--- app/assets/javascripts/admin/editor_page_contents.js
+++ app/assets/javascripts/admin/editor_page_contents.js
@@ -1,5 +1,16 @@
 //= require "trix"
-$(document).on('trix-before-initialize', () => {
-  console.log('boop');
-  Trix.config.blockAttributes.heading1.tagName = 'h2';
-});
+(function() {
+  $(document).on('trix-before-initialize', () => {
+    Trix.config.blockAttributes.heading1.tagName = 'h2';
+  });
+
+  function listenForAttachments() {
+    document.addEventListener('trix-attachment-add', (e) => {
+      console.log(e);
+    });
+  }
+
+  $(function() {
+    listenForAttachments();
+  });
+})();
