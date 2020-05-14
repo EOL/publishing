91c16ccc2efa261a605c09baf3cf432c9f74a2d7
Michael Vitale <mmvitale@gmail.com>
2020-05-14 16:36:24 -0400

    rename -> create column migration

--- db/migrate/20200511194724_rename_editor_pages_editor_content_directory_id.rb
+++ db/migrate/20200511194724_rename_editor_pages_editor_content_directory_id.rb
@@ -1,5 +1,5 @@
 class RenameEditorPagesEditorContentDirectoryId < ActiveRecord::Migration[5.2]
   def change
-    rename_column :editor_pages, :editor_content_directory_id, :editor_page_directory_id
+    add_column :editor_pages, :editor_page_directory_id, :integer
   end
 end
