68da0e61721129859f40280371ae6318acb5d0f8
Michael Vitale <mmvitale@gmail.com>
2020-05-14 12:54:40 -0400

    image upload integration w trix

--- app/assets/javascripts/admin/editor_page_contents.js
+++ app/assets/javascripts/admin/editor_page_contents.js
@@ -2,29 +2,49 @@
 (function() {
   $(document).on('trix-before-initialize', () => {
     Trix.config.blockAttributes.heading1.tagName = 'h2';
+    Trix.config.attachments.preview.caption = { name: false, size: false }
   });
 
   function listenForAttachments() {
     document.addEventListener('trix-attachment-add', (e) => {
-      if (e.attachment.file) {
-        uploadImage(e.attachment.file);
+      var attachment = e.attachment;
+
+      if (attachment.file) {
+        uploadImage(e.attachment.file, setProgress, setAttributes);
+      }
+
+      function setProgress(progress) {
+        attachment.setUploadProgress(progress); 
+      }
+
+      function setAttributes(attributes) {
+        attachment.setAttributes(attributes);
       }
     });
   }
 
-  function uploadImage(file) {
+  function uploadImage(file, onProgress, onSuccess) {
     var data = new FormData();
     data.append('image', file);
 
     $.ajax({
+      xhr: () => {
+        var xhr = new window.XMLHttpRequest();
+
+        // upload progress
+        xhr.upload.addEventListener("progress", function(event) {
+          var progress = event.loaded / event.total * 100
+          onProgress(progress)
+        }, false);
+
+        return xhr;
+      },
       url: 'upload_image',
       type: 'POST',
       data: data,
       processData: false,
       contentType: false,
-      success: (response) => {
-        console.log(response)
-      }
+      success: onSuccess
     })
   }
 
