bd91d56b51e5a88a32053fc8c925d68217270fd8
JRice <jrice@eol.org>
2020-06-10 10:53:27 -0400

    logger -> Rails.logger in decorator

--- app/decorators/page_decorator/brief_summary/conservation_status.rb
+++ app/decorators/page_decorator/brief_summary/conservation_status.rb
@@ -3,7 +3,7 @@ class PageDecorator
     class ConservationStatus
       attr_reader :page
       def initialize(page)
-        @page = page 
+        @page = page
       end
 
       def by_provider
@@ -26,13 +26,13 @@ class PageDecorator
                        elsif Eol::Uris::Conservation.usfw?(uri)
                          :usfw
                        else
-                         logger.warn("Unable to classify conservation status uri by provider: #{uri}")
+                         Rails.logger.warn("Unable to classify conservation status uri by provider: #{uri}")
                          nil
                        end
 
             if provider
               if @by_provider.include?(provider) && !multiples_warned.include?(provider)
-                logger.warn("Found multiple conservation status traits for page #{id}/provider #{provider}")
+                Rails.ogger.warn("Found multiple conservation status traits for page #{id}/provider #{provider}")
                 multiples_warned.add(provider)
               else
                 @by_provider[provider] = {
