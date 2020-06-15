0413eddc6a31447a098b1cb4d8386d1a1400506e
Michael Vitale <mmvitale@gmail.com>
2020-06-15 14:09:33 -0400

    Neography -> Neo4j exception class

--- app/controllers/service/cypher_controller.rb
+++ app/controllers/service/cypher_controller.rb
@@ -76,7 +76,7 @@ class Service::CypherController < ServicesController
     begin
       results = TraitBank.query(cypher)
       render_results(results, format)
-    rescue Neography::SyntaxException => e
+    rescue Neo4j::Core::CypherSession::CypherError => e
       return render_bad_request(title: "Cypher syntax error: #{e.message}")
     end
   end
