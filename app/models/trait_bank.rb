66b8c2dd863f57834d4a9765be0d872864a2416f
JRice <jrice@eol.org>
2020-06-17 12:36:06 -0400

    Just fixing a bad line break

--- app/models/trait_bank.rb
+++ app/models/trait_bank.rb
@@ -54,7 +54,7 @@ class TraitBank
       end
 
       hash_results = {}
-      hash_results["data"] = results.rows.collect do |row| 
+      hash_results["data"] = results.rows.collect do |row|
         row.collect do |val|
           if val.is_a? Hash
             val.stringify_keys
@@ -993,8 +993,7 @@ class TraitBank
     end
 
     def find_resource(id)
-      res = query("MATCH (resource:Resource { resource_id: #{id} }) "\
-        "RETURN resource LIMIT 1")
+      res = query("MATCH (resource:Resource { resource_id: #{id} }) RETURN resource LIMIT 1")
       res["data"] ? res["data"].first : false
     end
 
