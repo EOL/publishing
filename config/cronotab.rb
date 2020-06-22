595e33d257e39ef65ae55f8ed46241bd4bb6b6b3
JRice <jrice@eol.org>
2020-06-22 14:54:11 -0400

    Build sitemap every month.

--- config/cronotab.rb
+++ config/cronotab.rb
@@ -1,3 +1,7 @@
+require 'rake'
+
+Rails.app_class.load_tasks
+
 Crono.perform(WarmCacheJob).every 1.day, at: '3:00'
 Crono.perform(CommentsJob).every 1.day, at: '3:30'
 Crono.perform(CommentsJob).every 1.day, at: '13:30'
@@ -6,3 +10,4 @@ Crono.perform(TermsJob).every 1.day, at: '2:00'
 Crono.perform(PredCountJob).every 1.day, at: '1:00'
 Crono.perform(WarmCsvDownloadsJob).every 14.days, at: '1:00'
 Crono.perform(BuildIdentifierMapJob).every 1.month, on: 1
+Crono.perform(BuildSitemapJob).every 1.month, on: 1, at: { hour: 19 }
