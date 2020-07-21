6fd433a70dab70a892e0d50983356f72f52a3d70
JRice <jrice@eol.org>
2020-07-21 10:01:25 -0400

    Change to the way associations are built by helpers for views

--- app/controllers/concerns/data_associations.rb
+++ app/controllers/concerns/data_associations.rb
@@ -2,12 +2,11 @@ module DataAssociations
   extend ActiveSupport::Concern
 
   def build_associations(data)
-    @associations = 
-      begin
-        ids = data.map { |t| t[:object_page_id] }.compact.sort.uniq
-        pages = Page.where(id: ids).
-          includes(:medium, :preferred_vernaculars, native_node: [:rank])
-        pages.collect { |p| [p.id, p] }.to_h
-      end
+    begin
+      ids = data.map { |t| t[:object_page_id] }.compact.sort.uniq
+      pages = Page.where(id: ids).
+        includes(:medium, :preferred_vernaculars, native_node: [:rank])
+      pages.collect { |p| [p.id, p] }.to_h
+    end
   end
 end
