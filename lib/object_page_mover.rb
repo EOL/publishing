af83a46e0e7a6c352f42686e19a9dad4027ca956
Michael Vitale <mmvitale@gmail.com>
2020-08-10 16:55:07 -0400

    create ObjectPageMover util

---
+++ lib/object_page_mover.rb
@@ -0,0 +1,44 @@
+class ObjectPageMover
+  class << self
+    def run
+      create_rels
+      count_missed_rels
+    end
+
+    def create_rels
+      batch = 0
+      limit = 1000
+      count = 0
+
+      puts "creating missing relationships"
+
+      while batch == 0 || count > 0
+        puts "batch # #{batch} (of #{limit})"
+        q = %Q(
+          MATCH (t:Trait), (p:Page)
+          WHERE t.object_page_id IS NOT NULL AND p.page_id = t.object_page_id AND NOT (t)-[:object_page]->()
+          WITH t, p
+          ORDER BY t.eol_pk
+          SKIP #{batch * limit}
+          LIMIT #{limit}
+          CREATE (t)-[:object_page]->(p)
+          RETURN count(*)
+        )
+
+        result = TraitBank.query(q)
+        count = result["data"].first.first
+        batch += 1
+      end
+    end
+
+    def count_missed_rels
+      q = %q(
+        MATCH (t:Trait) WHERE t.object_page_id IS NOT NULL AND NOT (t)-[:object_page]->()
+        RETURN count(t), count(distinct t.object_page_id)
+      )
+
+      result = TraitBank.query(q)
+      puts "There are #{result["data"].first.first} traits with #{result["data"].first.second} unmatched object_page_ids"
+    end
+  end
+end
