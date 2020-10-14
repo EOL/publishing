195a41e4a3c983ff96d67a5520b678de66c11af0
Michael Vitale <mmvitale@gmail.com>
2020-10-14 11:06:35 -0400

    model concern for record/node (sql/neo4j) belongs_to relationship

---
+++ app/models/concerns/record_belongs_to_node.rb
@@ -0,0 +1,34 @@
+# Include in an ActiveRecord model to provide 'belongs_to_node' association with an ActiveGraph::Node model.
+# Example:
+#
+# include RecordBelongsToNode
+# belongs_to_node 'predicate', 'TermNode'
+#
+# where the calling class has a predicate_id field in its database table.
+module RecordBelongsToNode
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def belongs_to_node(assoc_name, class_name)
+      id_field_name = :"#{assoc_name}_id"
+      instance_var_name = "@#{assoc_name}"
+      klass = Object.const_get class_name
+
+      define_method(:"#{id_field_name}=") do |val|
+        if val != self.read_attribute(id_field_name)
+          self.write_attribute(id_field_name, val)
+          instance_variable_set(instance_var_name, nil)
+        end
+      end
+
+      define_method(assoc_name) do
+        cur_val = instance_variable_get(instance_var_name)
+        return cur_val if cur_val
+        instance_variable_set(
+          instance_var_name,
+          klass.find(self.read_attribute(id_field_name))
+        )
+      end
+    end
+  end
+end
