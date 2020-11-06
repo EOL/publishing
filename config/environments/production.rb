ce38ac5a3400d0d425921c75cee930a766bb6c57
JRice <jrice@eol.org>
2020-11-06 09:02:03 -0500

    Attempt to add version to Rails config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,3 +101,5 @@ Rails.configuration.repository_url = Rails.application.secrets.repository[:url]
 Rails.configuration.eol_web_url = Rails.application.secrets.host[:url]
 Rails.configuration.x.image_path = Rails.application.secrets.image_path
 Rails.configuration.traitbank_url = Rails.application.secrets.traitbank_url
+
+Rails.configuration.git_version = `cd #{Rails.root} && git rev-parse HEAD`
