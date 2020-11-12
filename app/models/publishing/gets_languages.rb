08e940c63854a3b0fcb0f27d4ca65a8f31064153
Michael Vitale <mmvitale@gmail.com>
2020-11-12 16:39:18 -0500

    update Publishing::GetsLanguages to use Locale

--- app/models/publishing/gets_languages.rb
+++ app/models/publishing/gets_languages.rb
@@ -1,11 +1,17 @@
 module Publishing::GetsLanguages
   def get_language(hash)
+    # Assumes that a given publishing run will have one language per group code
     @languages ||= {}
     return @languages[hash[:group_code]] if @languages.key?(hash[:group_code])
-    lang = Language.where(group: hash[:group_code]).first_or_create do |l|
-      l.group = hash[:group_code]
-      l.code = hash[:code]
+
+    locale = Locale.find_or_create_by(code: hash[:group_code])
+    lang = locale.languages.where(code: hash[:code])
+
+    if lang.nil?
+      lang = Language.create!(locale: locale, code: hash[:code])
     end
-    @languages[hash[:group_code]] = lang.id
+
+    @languages[hash[:group_code]] = lang
+    lang
   end
 end
