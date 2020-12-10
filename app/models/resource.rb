e17488f834bbe782f52e6d3f6d666af0c9ec7556
Michael Vitale <mmvitale@gmail.com>
2020-12-10 15:39:51 -0500

    remove obsolete publishing classes (& references)

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -407,21 +407,6 @@ class Resource < ApplicationRecord
     File.unlink(meta_traits_file) if File.exist?(meta_traits_file)
   end
 
-  def import_media(since)
-    log = Publishing::PubLog.new(self)
-    repo = Publishing::Repository.new(resource: self, log: log, since: since)
-    log.log('Importing Media ONLY...')
-    begin
-      Publishing::PubMedia.import(self, log, repo)
-      log.log('NOTE: Media have been loaded, but richness has not been recalculated, page icons aren''t updated, and '\
-        'media counts may be off.', cat: :infos)
-      log.complete
-    rescue => e
-      log.fail_on_error(e)
-    end
-    Rails.cache.clear
-  end
-
   def self.autocomplete(query, options = {})
     search(query, options.reverse_merge({
       fields: ['name'],
