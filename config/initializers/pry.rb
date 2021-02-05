47abc152a2e612367e4a91fa9be5e0830b0f1764
JRice <jrice@eol.org>
2021-02-05 10:31:20 -0500

    Updated pry config. A prompt now works...

--- config/initializers/pry.rb
+++ config/initializers/pry.rb
@@ -2,7 +2,11 @@
 # Show red environment name in pry prompt
 old_prompt = Pry.config.prompt
 env = Pry::Helpers::Text.red(Rails.env.upcase)
-Pry.config.prompt = [
-  proc { |*a| "#{env} #{old_prompt.first.call(*a)}" },
-  proc { |*a| "#{env} #{old_prompt.second.call(*a)}" }
-]
+Pry.config.prompt = Pry::Prompt.new(
+  'custom',
+  'a custom prompt for EOL',
+  [
+    proc { |*a| "#{env} #{old_prompt.wait_proc.call(*a)}" },
+    proc { |*a| "#{env} #{old_prompt.incomplete_proc.call(*a)}" }
+  ],
+)
