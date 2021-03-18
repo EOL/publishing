3eae465dc1bb09f49838350d3d60764b9f723681
Michael Vitale <mmvitale@gmail.com>
2021-03-18 13:50:09 -0400

    detailed report for circular relationships

--- app/models/data_integrity_check/circular_relationships.rb
+++ app/models/data_integrity_check/circular_relationships.rb
@@ -2,14 +2,40 @@
 class DataIntegrityCheck::CircularRelationships
   include DataIntegrityCheck::ZeroCountCheck
 
+  def detailed_report
+    query = <<~CYPHER
+      #{query_common}
+      RETURN t1.uri AS uri1, t2.uri AS uri2
+    CYPHER
+
+    result = ActiveGraph::Base.query(query).to_a
+    display_result = result.map do |r|
+      '[' + [r[:uri1], r[:uri2]].join(', ') + ']'
+    end.join("\n")
+
+    <<~END
+      Query:
+      #{query}
+      
+      Result:
+      #{display_result}
+    END
+  end
+
   private
-  def query
-    %{
+  def query_common
+    <<~CYPHER
       MATCH (t1:Term)-[:parent_term|synonym_of*1..]->(t2:Term)
       WHERE (t2)-[:parent_term|synonym_of*0..]->(t1) AND t1 <> t2
       WITH DISTINCT t1, t2
+    CYPHER
+  end
+
+  def query
+    <<~CYPHER
+      #{query_common}
       RETURN count(*) / 2 AS count
-    }
+    CYPHER
   end
 
   def build_count_message(count)
