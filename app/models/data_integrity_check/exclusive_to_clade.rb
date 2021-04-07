c1d34f223de83e5bc8a1474b75a3179aece9d205
Michael Vitale <mmvitale@gmail.com>
2021-04-07 15:55:41 -0400

    detailed report queries for exclusive/incompatible clade checks

--- app/models/data_integrity_check/exclusive_to_clade.rb
+++ app/models/data_integrity_check/exclusive_to_clade.rb
@@ -1,13 +1,27 @@
 class DataIntegrityCheck::ExclusiveToClade
   include DataIntegrityCheck::ZeroCountCheck
+  include DataIntegrityCheck::HasDetailedReport
 
   private
   def query
     <<~CYPHER
+      #{query_common}
+      RETURN count(distinct trait) AS count
+    CYPHER
+  end
+
+  def detailed_report_query
+    <<~CYPHER
+      #{query_common}
+      RETURN distinct page.page_id, trait.eol_pk, predicate.uri, clade.page_id
+    CYPHER
+  end
+
+  def query_common
+    <<~CYPHER
       MATCH (page:Page)-[:trait]->(trait:Trait),
       (trait)-[:predicate]->(predicate:Term)-[:exclusive_to_clade]->(clade:Page)
       WHERE NOT (page)-[:parent*0..]->(clade)
-      RETURN count(distinct trait) AS count
     CYPHER
   end
 
--- app/models/data_integrity_check/incompatible_with_clade.rb
+++ app/models/data_integrity_check/incompatible_with_clade.rb
@@ -1,15 +1,30 @@
 class DataIntegrityCheck::IncompatibleWithClade
   include DataIntegrityCheck::ZeroCountCheck
+  include DataIntegrityCheck::HasDetailedReport
 
   private
   def query
     <<~CYPHER
-      MATCH (page:Page)-[:trait]->(trait:Trait),
-      (trait)-[:object_term]->(object_term:Term)-[:incompatible_with_clade]->(clade:Page)
-      WHERE (page)-[:parent*0..]->(clade)
+      #{query_common}
       RETURN count(distinct trait) AS count
     CYPHER
   end
+
+  def detailed_report_query
+    <<~CYPHER
+      #{query_common}
+      RETURN page.page_id, trait.eol_pk, object_term.uri, clade.page_id
+    CYPHER
+  end
+
+  def query_common
+    <<~CYPHER
+      MATCH (page:Page)-[:trait]->(trait:Trait),
+      (trait)-[:object_term]->(object_term:Term)-[:incompatible_with_clade]->(clade:Page)
+      WHERE (page)-[:parent*0..]->(clade)
+    CYPHER
+  end
+
 
   def build_count_message(count)
     "Found #{count} trait(s) with incompatible_with_clade object_terms where the trait's page is a member of the incompatible clade"
