4504ffbc293a4083a1dc76163bc84475453e0c28
Michael Vitale <mmvitale@gmail.com>
2021-04-07 16:33:55 -0400

    update detailed report query for clade/term checks

--- app/models/data_integrity_check/exclusive_to_clade.rb
+++ app/models/data_integrity_check/exclusive_to_clade.rb
@@ -2,26 +2,29 @@ class DataIntegrityCheck::ExclusiveToClade
   include DataIntegrityCheck::ZeroCountCheck
   include DataIntegrityCheck::HasDetailedReport
 
+  def self.show_detailed_report_on_pass?
+    true
+  end
+
   private
   def query
     <<~CYPHER
-      #{query_common}
+      MATCH (page:Page)-[:trait]->(trait:Trait),
+      (trait)-[:predicate]->(predicate:Term)-[:exclusive_to_clade]->(clade:Page)
+      WHERE NOT (page)-[:parent*0..]->(clade)
       RETURN count(distinct trait) AS count
     CYPHER
   end
 
   def detailed_report_query
     <<~CYPHER
-      #{query_common}
-      RETURN distinct page.page_id, trait.eol_pk, predicate.uri, clade.page_id
-    CYPHER
-  end
-
-  def query_common
-    <<~CYPHER
       MATCH (page:Page)-[:trait]->(trait:Trait),
+      (trait)-[:supplier]->(resource:Resource),
       (trait)-[:predicate]->(predicate:Term)-[:exclusive_to_clade]->(clade:Page)
       WHERE NOT (page)-[:parent*0..]->(clade)
+      WITH resource.resource_id AS resource_id, predicate.uri AS predicate_uri, count(distinct trait) AS trait_count
+      RETURN resource_id, predicate_uri, trait_count
+      ORDER BY resource_id, trait_count DESC
     CYPHER
   end
 
--- app/models/data_integrity_check/incompatible_with_clade.rb
+++ app/models/data_integrity_check/incompatible_with_clade.rb
@@ -2,29 +2,31 @@ class DataIntegrityCheck::IncompatibleWithClade
   include DataIntegrityCheck::ZeroCountCheck
   include DataIntegrityCheck::HasDetailedReport
 
+  def self.show_detailed_report_on_pass?
+    true
+  end
+
   private
   def query
     <<~CYPHER
-      #{query_common}
+      MATCH (page:Page)-[:trait]->(trait:Trait),
+      (trait)-[:object_term]->(object_term:Term)-[:incompatible_with_clade]->(clade:Page)
+      WHERE (page)-[:parent*0..]->(clade)
       RETURN count(distinct trait) AS count
     CYPHER
   end
 
   def detailed_report_query
     <<~CYPHER
-      #{query_common}
-      RETURN page.page_id, trait.eol_pk, object_term.uri, clade.page_id
-    CYPHER
-  end
-
-  def query_common
-    <<~CYPHER
       MATCH (page:Page)-[:trait]->(trait:Trait),
+      (trait)-[:supplier]->(resource:Resource),
       (trait)-[:object_term]->(object_term:Term)-[:incompatible_with_clade]->(clade:Page)
       WHERE (page)-[:parent*0..]->(clade)
+      WITH resource.resource_id AS resource_id, object_term.uri AS object_uri, count(distinct trait) AS trait_count
+      RETURN resource_id, object_uri, trait_count
+      ORDER BY resource_id, trait_count DESC
     CYPHER
   end
-
 
   def build_count_message(count)
     "Found #{count} trait(s) with incompatible_with_clade object_terms where the trait's page is a member of the incompatible clade"
