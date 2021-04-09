b61f075b12e92b2c764835205dff56c7429a1a1b
Michael Vitale <mmvitale@gmail.com>
2021-04-09 15:23:12 -0400

    remove unmapped pages from global validity check

--- app/models/data_integrity_check/exclusive_to_clade.rb
+++ app/models/data_integrity_check/exclusive_to_clade.rb
@@ -7,7 +7,7 @@ class DataIntegrityCheck::ExclusiveToClade
     <<~CYPHER
       MATCH (page:Page)-[:trait]->(trait:Trait),
       (trait)-[:predicate]->(predicate:Term)-[:exclusive_to_clade]->(clade:Page)
-      WHERE NOT (page)-[:parent*0..]->(clade)
+      WHERE (page)-[:parent]->(:Page) AND NOT (page)-[:parent*0..]->(clade)
       RETURN count(distinct trait) AS count
     CYPHER
   end
@@ -17,7 +17,7 @@ class DataIntegrityCheck::ExclusiveToClade
       MATCH (page:Page)-[:trait]->(trait:Trait),
       (trait)-[:supplier]->(resource:Resource),
       (trait)-[:predicate]->(predicate:Term)-[:exclusive_to_clade]->(clade:Page)
-      WHERE NOT (page)-[:parent*0..]->(clade)
+      WHERE (page)-[:parent]->(:Page) AND NOT (page)-[:parent*0..]->(clade)
       WITH resource.resource_id AS resource_id, predicate.uri AS predicate_uri, count(distinct trait) AS trait_count
       RETURN resource_id, predicate_uri, trait_count
       ORDER BY resource_id, trait_count DESC
