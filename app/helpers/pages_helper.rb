1416596c9f1a59a22da1276ac2c591c855faf290
JRice <jrice@eol.org>
2021-05-25 20:42:32 -0400

    Experimental fix ... low expectations of it working...

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -110,7 +110,7 @@ module PagesHelper
 
   def classification_siblings(page_node)
     string = ''
-    sort_nodes_by_name(page_node.siblings[0..99]).each do |sibling|
+    sort_nodes_by_name(page_node.siblings.includes(:vernacular, :preferred_vernaculars)[0..99]).each do |sibling|
       string << %Q{<div class="item">}
       string << summarize(sibling.page, name: sibling.scientific_name, current_page: false, node: sibling,
         no_icon: true)
