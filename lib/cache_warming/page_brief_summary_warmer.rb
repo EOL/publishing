07d6fc277e4769d2758c32486de599d01297f91e
Michael Vitale <mmvitale@gmail.com>
2021-05-28 11:52:01 -0400

    add timestamps to BriefSummaryWarmer logging

--- lib/cache_warming/page_brief_summary_warmer.rb
+++ lib/cache_warming/page_brief_summary_warmer.rb
@@ -2,13 +2,13 @@ module CacheWarming
   module PageBriefSummaryWarmer
     class << self
       def run
-        puts "Calling brief summary for all pages"
+        log "Calling brief summary for all pages"
 
         # Only for 'en'
         I18n.locale = I18n.default_locale
 
         Page.with_hierarchy_no_media.where.not(native_node_id: nil).find_in_batches.with_index do |batch, i|
-          puts "Processing batch #{i}"
+          log "Processing batch #{i}"
           decorated_batch = PageDecorator.decorate_collection(batch)
 
           decorated_batch.each do |p|
@@ -16,12 +16,16 @@ module CacheWarming
               p.cached_summary
             rescue ActiveGraph::Node::Labels::RecordNotFound
               # I had some missing neo4j PageNodes that threw this -- skip 'em and move on
-              puts "WARNING: missing PageNode with id #{p.id}. Skipping"
+              log "WARNING: missing PageNode with id #{p.id}. Skipping"
             end
           end
         end
 
         puts "Done"
+      end
+
+      def log(msg)
+        puts "(#{Time.now}) #{msg}"
       end
     end
   end
