0f50fe74ff70897b1f304894c737b5acae0ce430
Michael Vitale <mmvitale@gmail.com>
2021-06-02 15:49:40 -0400

    switch cors from nginx to rack-cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/api/reconciliation*', headers: :any, methods: [:get, :post]
+    resource '*', 
+      headers: :any, 
+      methods: [:get, :post],
+      expose: ['Content-Length', 'Content-Range']
+    resource '*', 
+      headers: :any, 
+      methods: [:options],
+      max_age: 1728000 # Tell client that this pre-flight info is valid for 20 days
   end
 end
