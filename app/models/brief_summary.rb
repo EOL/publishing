0f755f48316b7c6733789e67d23503af7c249c05
Michael Vitale <mmvitale@gmail.com>
2021-06-21 12:15:59 -0400

    move brief summary code to app/models

---
+++ app/models/brief_summary.rb
@@ -0,0 +1,18 @@
+# At the time of writing, this was an implementation of
+# https://github.com/EOL/eol_website/issues/5#issuecomment-397708511 and
+# https://github.com/EOL/eol_website/issues/5#issuecomment-402848623
+
+class BriefSummary
+  def initialize(page, view)
+    @page = page
+    @view = view
+    @adapter = I18n.locale == I18n.default_locale ? 
+      English.new(@page, @view) :
+      OtherLanguages.new(@page, @view)
+  end
+
+  def result
+    @adapter.result
+  end
+end
+
