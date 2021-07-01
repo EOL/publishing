a74fb6a787d103e222d386ee4fa740dddfa06ff0
Michael Vitale <mmvitale@gmail.com>
2021-07-01 11:53:49 -0400

    fix nil page reconciliation api bug

--- app/models/reconciliation_result/scored_page.rb
+++ app/models/reconciliation_result/scored_page.rb
@@ -12,20 +12,22 @@ class ReconciliationResult
     end
 
     class << self
-      def from_searchkick_results(query_string, results)
+      def from_searchkick_results(query_string, searchkick_results)
+        results = process_searchkick_results(searchkick_results)
         return [] if results.empty?
 
-        best_match = results.first
+        best_match_result = results.first
+        best_match = best_match_result.page
         highlight_strs = best_match.search_highlights.values.map { |v| v.downcase }
         best_score_abs = highlight_strs.include?(query_string.downcase) ? ReconciliationResult::MAX_SCORE : INEXACT_TOP_SCORE
-        best_score_rel = results.hits.first['_score']
-        worst_score_rel = results.hits.last['_score']
+        best_score_rel = best_match_result.score
+        worst_score_rel = results.last.score
         rel_range = best_score_rel - worst_score_rel
         abs_range = best_score_abs - MIN_NAME_SCORE
         abs_rel_ratio = rel_range > 0 ? abs_range * 1.0 / rel_range : 0 # 0 is a sentinel value, won't be used in calculations
 
-        results.hits.map.with_index do |hit, i|
-          score_rel = hit['_score']
+        results.map.with_index do |result, i|
+          score_rel = result.score
           score_abs = abs_rel_ratio > 0 ? (score_rel - worst_score_rel) * abs_rel_ratio + MIN_NAME_SCORE : best_score_abs
 
           # First result is a confident match if 
@@ -33,12 +35,31 @@ class ReconciliationResult
           # 2) the next result has a lower score (or doesn't exist)
           confident_match = (
             i == 0 && 
-            (results.length == 1 || results.hits[1]['_score'] < score_rel) &&
+            (results.length == 1 || results.second.score < score_rel) &&
             best_score_abs == MAX_SCORE
           )
 
-          self.new(results[i], score_abs, confident_match)
+          self.new(result.page, score_abs, confident_match)
+        end.compact
+      end
+
+      private
+      InternalResult = Struct.new(:page, :score)
+
+      def process_searchkick_results(searchkick_results)
+        internal_results = []
+
+        pages = searchkick_results.results
+        hits = searchkick_results.hits
+
+        pages.each_with_index do |page, i|
+          if page
+            internal_results << InternalResult.new(page, hits[i]['_score'])
+          end
+          # if elasticsearch is out of sync with the db (index out of date), page can be nil, and we should ignore such results
         end
+
+        internal_results
       end
     end
   end
