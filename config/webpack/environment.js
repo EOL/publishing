979713c33df7fe45473290fa6e97e4f8337003a0
Jeremy Rice (Windows PC) <jrice@eol.org>
2021-07-13 10:10:39 -0400

    Hopefully silence some webpacker warnings...

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+environment.config.merge({
+  performance: {
+    maxEntrypointSize: 300000,
+    maxAssetSize: 300000
+  }
+})
+
 module.exports = environment
