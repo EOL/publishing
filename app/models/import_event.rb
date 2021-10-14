c8c2d7550ac8d0b1a57f43ccb4f68dccd0888d7f
Jeremy Rice (Windows PC) <jrice@eol.org>
2021-10-14 12:20:06 -0400

    I neglected to finish a logging feature called log_update, this should do it.

--- app/models/import_event.rb
+++ app/models/import_event.rb
@@ -1,7 +1,7 @@
 class ImportEvent < ApplicationRecord
   belongs_to :import_log, inverse_of: :import_events
 
-  enum cat: %i[infos warns errors starts ends urls]
+  enum cat: %i[infos warns errors starts ends urls updates]
 
   scope :warns, -> { where(cat: ImportEvent.cats[:warns]) }
 end
