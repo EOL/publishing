3d303b335df8834576033392d52bf4c35e5b6aba
Jeremy Rice (Win box) <jrice@eol.org>
2022-06-02 13:57:28 -0400

    rebuild supervisor.sock

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-rm -f /tmp/*.pid /tmp/*.sock
-rm -rf /tmp/cache/assets
+rm -f /app/tmp/*.pid tmp/*.sock
+rm -rf tmp/cache/assets
+touch /tmp/supervisor.sock
+chmod 777 /tmp/supervisor.sock
 bundle install
 bundle update eol_terms
 rake assets:precompile
