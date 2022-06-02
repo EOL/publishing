aebf4d834e763a11fa8e16cb4a747e9651211801
Jeremy Rice (Win box) <jrice@eol.org>
2022-06-02 14:13:24 -0400

    bundle update?

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,7 +3,7 @@ rm -f /app/tmp/*.pid tmp/*.sock
 rm -rf tmp/cache/assets
 touch /tmp/supervisor.sock
 chmod 777 /tmp/supervisor.sock
-bundle install
+bundle update
 bundle update eol_terms
 rake assets:precompile
 exec "$@"
