efb6a467e2a24a561cbbfa182691d184553b4cfe
Jeremy Rice (Win box) <jrice@eol.org>
2022-06-10 17:47:17 -0400

    Don't try to import missing js_routes config

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -3,7 +3,6 @@ import '../src/main'
 import '../src/youtube'
 import '@client-side-validations/client-side-validations'
 import '@client-side-validations/simple-form'
-import '../src/js_route_config.js.erb'
 import I18n from '../i18n-js/index.js.erb'
 
 window.I18n = I18n
