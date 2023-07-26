80d52057ac6b20d3c9c97cdd94480a2cac4074ac
JRice <jrice@eol.org>
2023-07-26 11:21:50 -0400

    Updates to the media_manifest to make the tgz file

--- app/models/medium/manifest_exporter.rb
+++ app/models/medium/manifest_exporter.rb
@@ -13,29 +13,39 @@ class Medium
                     'Copyright Owner']]
                 puts "start #{Time.now}"
                 STDOUT.flush
+                subclasses = [Medium.subclasses[:image], Medium.subclasses[:map_image]]
                 # NOTE: this no longer restricts itself to visible or trusated media, but I think that's fine for the use-case.
-                Medium.where('page_id IS NOT NULL').includes(:license).find_each do |item|
+                Medium.where('page_id IS NOT NULL').where(subclass: subclasses).includes(:license).find_each do |item|
                     begin
-                    @collection << [item.id, item.page_id, item.source_url, item.original_size_url, item.license&.name, item&.owner]
+                        @collection << [item.id, item.page_id, item.source_url, item.original_size_url, item.license&.name, item&.owner]
                     rescue => e
-                    puts "FAILED on page item #{item.id} (#{item.resource.name})"
-                    puts "ERROR: #{e.message}"
-                    STDOUT.flush
+                        puts "FAILED on page item #{item.id} (#{item.resource.name})"
+                        puts "ERROR: #{e.message}"
+                        STDOUT.flush
                     end
                     flush_collection if @collection.size >= 100_000
                 end
-                puts "end #{Time.now}"
+                puts "created files #{Time.now} ... now archiving..."
                 flush_collection unless @collection.empty?
+                zip_collections
+                puts "end #{Time.now}"
             end
 
             def flush_collection
-                CSV.open(Rails.root.join('public', 'data', "media_manifest_#{@collection_num}.csv"), 'wb') do |csv|
+                file = Rails.public_path.join('data', "media_manifest_#{@collection_num}.csv")
+                CSV.open(file, 'wb') do |csv|
                     @collection.each { |row| csv << row }
                 end
                 @collection = []
-                puts "flushed ##{@collection_num} @ #{Time.now}"
+                puts "flushed ##{@collection_num} @ #{Time.now} > #{file}"
                 STDOUT.flush
                 @collection_num += 1
+            end
+
+            def zip_collections
+                zipfile = Rails.public_path.join('data', 'media_manifest.tgz')
+                File.unlink(zipfile) if File.exist?(zipfile)
+                `tar cvzf #{zipfile} #{Rails.public_path.join('data')}/media_manifest_*.csv`
             end
         end
     end
