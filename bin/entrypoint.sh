0c55400d78b785ca64d7e762c3b2b43c1f7cad98
Jeremy Rice (Win box) <jrice@eol.org>
2024-02-26 13:36:38 -0500

    Improve entrypoint I hope

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set 
 rm -f /app/tmp/*.pid tmp/*.sock
 rm -rf tmp/cache/assets
 touch /tmp/supervisor.sock
@@ -6,6 +7,8 @@ chmod 777 /tmp/supervisor.sock
 gem install `tail -n 1 Gemfile.lock | sed 's/^\s\+/bundler:/'`
 bundle update
 bundle update eol_terms
+export NODE_OPTIONS='--openssl-legacy-provider npm run start'
+yarn install
 yarn upgrade > /app/log/assets.log 2>&1
 /app/bin/webpack >> /app/log/assets.log 2>&1
 rake assets:precompile >> /app/log/assets.log 2>&1
