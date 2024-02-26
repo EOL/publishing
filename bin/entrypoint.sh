ab80efaff9cd479f21dfdd884e7c4e33b294a805
Jeremy Rice (Win box) <jrice@eol.org>
2024-02-26 13:43:13 -0500

    move the yarn install command in entrypoint

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -8,8 +8,8 @@ gem install `tail -n 1 Gemfile.lock | sed 's/^\s\+/bundler:/'`
 bundle update
 bundle update eol_terms
 export NODE_OPTIONS='--openssl-legacy-provider npm run start'
-yarn install
 yarn upgrade > /app/log/assets.log 2>&1
 /app/bin/webpack >> /app/log/assets.log 2>&1
+yarn install > /app/log/assets.log 2>&1
 rake assets:precompile >> /app/log/assets.log 2>&1
 exec "$@"
