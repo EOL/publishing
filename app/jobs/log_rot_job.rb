ac4ba6f8773f43bef0d202777b9738fc838e5d00
Jeremy Rice (Win box) <jrice@eol.org>
2024-03-19 07:44:14 -0400

    Adjust logrotate task

--- app/jobs/log_rot_job.rb
+++ app/jobs/log_rot_job.rb
@@ -1,5 +1,5 @@
 class LogRotJob < ApplicationJob
   def perform
-    `logrotate #{Rails.root.join('config', 'logrotate.conf')}`
+    `/usr/sbin/logrotate #{Rails.root.join('config', 'logrotate.conf')} >> #{Rails.root.join('log', 'logrotate.log')} 2>&1`
   end
 end
