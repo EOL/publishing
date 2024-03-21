95f05d3448792d2332628e9a3e89baa957d6ae11
Jeremy Rice (Win box) <jrice@eol.org>
2024-03-21 13:25:55 -0400

    clean up unicorn conf

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,16 +2,11 @@ app_dir = "/app"
 
 working_directory app_dir
 
-pid "#{app_dir}/tmp/pids/unicorn.pid"
-
 stderr_path "#{app_dir}/log/unicorn.stderr.log"
 # STDOUT turns out to be pretty useless: it just prints "{:join=>", ", :open=>"(", :close=>")"}" over and over. :|
 # NOTE: the default, if missing, is /dev/null ...which we want in this case.
 # stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
-# Going to try "scale" from docker instead:
-# worker_processes ENV['WORKER_PROCESSES'].to_i
-# listen "#{app_dir}/tmp/unicorn.sock", :backlog => 1024
 listen 9393
 timeout 905 # Setting this HIGHER than unicorn, so that we don't reap processes unless we have to.
 
