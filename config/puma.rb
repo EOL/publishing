7f20b94b0960b8c4db489ae6f8bae17d41e6eae7
Jeremy Rice (Win box) <jrice@eol.org>
2024-05-09 19:24:39 -0400

    removing a problem method from puma init

--- config/puma.rb
+++ config/puma.rb
@@ -17,14 +17,17 @@ rackup      DefaultRackup if defined?(DefaultRackup)
 port        ENV['PORT']     || 3000
 environment ENV['RAILS_ENV'] || 'development'
 
-on_worker_boot do
-  puts "Worker booting..."
-  if defined?(ActiveGraph::Base)
-    config = Rails.application.config.neo4j.driver
-    ActiveGraph::Base.driver = Neo4j::Driver::GraphDatabase.driver(
-      config.url,
-      Neo4j::Driver.AuthTokens.basic(config.username, config.password),
-      encryption: config.encryption
-    )
-  end
-end
+# JRice: I'm removing this because it throws { undefined method `AuthTokens' for module Neo4j::Driver } and
+# I don't really want to muck around with fixing that right now.
+#
+# on_worker_boot do
+#   puts "Worker booting..."
+#   if defined?(ActiveGraph::Base)
+#     config = Rails.application.config.neo4j.driver
+#     ActiveGraph::Base.driver = Neo4j::Driver::GraphDatabase.driver(
+#       config.url,
+#       Neo4j::Driver.AuthTokens.basic(config.username, config.password),
+#       encryption: config.encryption
+#     )
+#   end
+# end
