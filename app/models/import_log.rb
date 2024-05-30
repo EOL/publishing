3c32b66ca7a2842feda487ff8b7d685f6748f72f
Jeremy Rice (Win box) <jrice@eol.org>
2024-05-29 20:22:12 -0400

    ImportLog to STDOUT, too

--- app/models/import_log.rb
+++ app/models/import_log.rb
@@ -45,6 +45,7 @@ class ImportLog < ApplicationRecord
     body = "#{call_stack.join('>')}= #{body}"
     chop_into_text_chunks(body).each do |chunk|
       import_events << ImportEvent.create(import_log: self, cat: cat, body: chunk)
+      puts "IMPORT #{cat}: #{chunk}"
     end
   end
 
