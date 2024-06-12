d8de0a4dba420d8014686b162117cef9c56fbe36
Jeremy Rice (Win box) <jrice@eol.org>
2024-06-12 10:18:44 -0400

    This should generate three types of media manifest

--- app/models/medium/manifest_exporter.rb
+++ app/models/medium/manifest_exporter.rb
@@ -4,6 +4,39 @@ class Medium
             def export
                 require 'csv'
                 @collection_num = 1
+                @types = {
+                    'video' => [Medium.subcategories[:video]],
+                    'media' => [Medium.subcategories[:image], Medium.subcategories[:map_image]],
+                    'audio' => [Medium.subcategories[:sound]]
+                }
+                puts "start #{Time.now}"
+                STDOUT.flush
+                @types.keys.each do |type|
+                    reset_collection
+                    puts "handling #{type} manifest..."
+                    remove_existing_files(type)
+                    # NOTE: this no longer restricts itself to visible or trusated media, but I think that's fine for the use-case.
+                    Medium.where('page_id IS NOT NULL').where(subcategory: @types[type]).includes(:license).find_each do |item|
+                        begin
+                            @collection << [item.id, item.page_id, item.source_url, item.original_size_url, item.license&.name, item&.owner]
+                        rescue => e
+                            puts "FAILED on page item #{item.id} (#{item.resource.name})"
+                            puts "ERROR: #{e.message}"
+                            STDOUT.flush
+                        end
+                        flush_collection(type) if @collection.size >= 100_000
+                    end
+                    puts "created files #{Time.now} ... now archiving..."
+                    flush_collection(type) unless @collection.empty?
+                    zipfile = zip_collections(type)
+                end
+                puts "updating timestamp on OpenData..."
+                update_opendata_timestamp
+                puts "end #{Time.now}"
+                return zipfile
+            end
+
+            def reset_collection
                 @collection = [[
                     'EOL content ID',
                     'EOL page ID',
@@ -11,39 +44,17 @@ class Medium
                     'EOL Full-Size Copy URL',
                     'License Name',
                     'Copyright Owner']]
-                puts "start #{Time.now}"
-                STDOUT.flush
-                remove_existing_files
-                subcategories = [Medium.subcategories[:image], Medium.subcategories[:map_image]]
-                # NOTE: this no longer restricts itself to visible or trusated media, but I think that's fine for the use-case.
-                Medium.where('page_id IS NOT NULL').where(subcategory: subcategories).includes(:license).find_each do |item|
-                    begin
-                        @collection << [item.id, item.page_id, item.source_url, item.original_size_url, item.license&.name, item&.owner]
-                    rescue => e
-                        puts "FAILED on page item #{item.id} (#{item.resource.name})"
-                        puts "ERROR: #{e.message}"
-                        STDOUT.flush
-                    end
-                    flush_collection if @collection.size >= 100_000
-                end
-                puts "created files #{Time.now} ... now archiving..."
-                flush_collection unless @collection.empty?
-                zipfile = zip_collections
-                puts "updating timestamp on OpenData..."
-                update_opendata_timestamp
-                puts "end #{Time.now}"
-                return zipfile
             end
 
-            def remove_existing_files
-                glob = Dir.glob(Rails.public_path.join('data', 'media_manifest_*.csv'))
+            def remove_existing_files(type)
+                glob = Dir.glob(Rails.public_path.join('data', "#{type}_manifest_*.csv"))
                 return if glob.empty?
                 puts "removing #{glob.size} files ..."
                 glob.each { |file| File.delete(file) }
             end
 
-            def flush_collection
-                file = Rails.public_path.join('data', "media_manifest_#{@collection_num}.csv")
+            def flush_collection(type)
+                file = Rails.public_path.join('data', "#{type}_manifest_#{@collection_num}.csv")
                 CSV.open(file, 'wb') do |csv|
                     @collection.each { |row| csv << row }
                 end
@@ -53,11 +64,11 @@ class Medium
                 @collection_num += 1
             end
 
-            def zip_collections
+            def zip_collections(type)
                 dir = Rails.public_path.join('data')
-                zipfile = Rails.public_path.join('data', 'media_manifest.tgz')
+                zipfile = Rails.public_path.join('data', "#{type}_manifest.tgz")
                 File.unlink(zipfile) if File.exist?(zipfile)
-                `cd #{dir} && tar cvzf #{zipfile} media_manifest_*.csv`
+                `cd #{dir} && tar cvzf #{zipfile} #{type}_manifest_*.csv`
                 return zipfile
             end
 
