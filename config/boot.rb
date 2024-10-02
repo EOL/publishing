d8916ab612bb04b8e8f439115ff713d871a40971
JRice (Ubuntu Tower) <jrice@eol.org>
2024-10-02 16:00:47 -0400

    Restructuring boot errors

--- config/boot.rb
+++ config/boot.rb
@@ -5,18 +5,20 @@ require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 
 begin
   require 'active_graph'
-rescue
+rescue => e
   puts "ERROR: Neo4j/active_graph failed to connect! (#{e.class} - #{e.message})"
 end
 
 begin
   require 'neo4j_ruby_driver'
   # Nesting this b/c the error type doesn't exist until the module is required!
-  begin 
-    ActiveGraph::Migrations.check_for_pending_migrations!
-  rescue Neo4j::Driver::Exceptions::SessionExpiredException => e
-    puts "ERROR: Neo4j migrations pending! (#{e.class} - #{e.message})"
-  end
-rescue
+rescue => e
   puts "ERROR: Neo4j/neo4j-ruby-driver failed to connect! (#{e.class} - #{e.message})"
+end
+
+begin 
+  ActiveGraph::Migrations.check_for_pending_migrations!
+rescue => e
+  puts "ERROR: Neo4j migrations pending!"
+  puts "ERROR: (#{e.class} - #{e.message})" if e
 end
