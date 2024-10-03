d09b24ec649b3947801ef337c0b2bed2cc450759
JRice (Ubuntu Tower) <jrice@eol.org>
2024-10-03 16:13:39 -0400

    monkey-patch around a bug in ActiveGraph

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,16 @@ require_relative 'application'
 # "/gems/ruby/3.3.0/gems/activegraph-10.1.1/config/neo4j/config.yml"
 # ...which is no good for us, so we set it here:
 ActiveGraph::Config.default_file = "/app/config/neo4j/config.yml"
+# When that's set, apparently this never gets set, soooo:
+module ActiveGraph
+  module Migrations
+    class << self
+      def currently_running_migrations
+        return true # If it's NOT true, it's goping to validate them, EVEN THOUGH WE CONFIGED IT NOT TO.
+      end
+    end
+  end
+end
 
 # Initialize the Rails application.
 Rails.application.initialize!
