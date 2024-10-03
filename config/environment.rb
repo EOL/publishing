d4f52a4ade5c099d4f500e856450df4386ab8135
JRice (Ubuntu Tower) <jrice@eol.org>
2024-10-03 16:03:42 -0400

    ActiveGraph::Config.default_file was wrong

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
 
+# For whatever reason, this defaults to within the GEM directory, e.g.:
+# "/gems/ruby/3.3.0/gems/activegraph-10.1.1/config/neo4j/config.yml"
+# ...which is no good for us, so we set it here:
+ActiveGraph::Config.default_file = "/app/config/neo4j/config.yml"
+
 # Initialize the Rails application.
 Rails.application.initialize!
