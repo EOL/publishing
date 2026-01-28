038142d9d3d5b696e1f90d69d5cbefb32f8e9eb0
Jeremy Rice (PC) <jrice@eol.org>
2026-01-28 14:14:54 -0500

    Fix for missing logger

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'logger' # Fix for Ruby 3.3 / Rails 6.1 compatibility
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 
 module ActiveGraph
