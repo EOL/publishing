65d229aabad41307605602aec51cb73a5c48af3a
Jeremy Rice (Win 11 Tower) <jrice.blue@gmail.com>
2026-05-29 12:35:05 -0400

    entrypoint now has to manage rails secrets

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,6 +2,25 @@
 set 
 rm -f /app/tmp/*.pid /app/tmp/pids/*.pid tmp/*.sock
 rm -rf tmp/cache/assets
+
+set -e
+
+file_env() {
+  var="$1"
+  file_var="${var}_FILE"
+  eval file_path="\${$file_var:-}"
+
+  if [ -n "$file_path" ] && [ -f "$file_path" ]; then
+    export "$var=$(cat "$file_path")"
+  fi
+}
+
+file_env RAILS_MASTER_KEY
+file_env TRAITBANK_URL
+file_env NEO4J_USER
+file_env NEO4J_PASSWORD
+file_env SCOUT_APM_KEY
+
 bundle update eol_terms
 
 exec "$@"
