35a46542339c2aba1b9a5db68dfbfec288854599
Jeremy Rice (Win 11 Tower) <jrice.blue@gmail.com>
2026-06-30 10:50:21 -0400

    Fixing a boot load order problem.

--- config/boot.rb
+++ config/boot.rb
@@ -2,6 +2,13 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
+# Ruby 3.3+ no longer auto-loads `logger` (it moved from a default to a bundled
+# gem), but ActiveSupport 6.1 references the `Logger` constant at load time
+# before requiring it. Load it here, before any Rails framework, to avoid an
+# "uninitialized constant ActiveSupport::LoggerThreadSafeLevel::Logger" crash.
+# REMOVE THIS WHEN YOU UPDATE TO RAILS 7.
+require 'logger'
+
 module ActiveGraph
   module Migrations
     class << self
