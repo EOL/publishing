47fae327d48549e7c9424ef966ea039803cda6c3
Jeremy Rice (Win 11 Tower) <jrice.blue@gmail.com>
2026-07-29 14:28:47 -0400

    Change puma timeout to  PUMA_WORKER_TIMEOUT (default 60)

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@ environment ENV.fetch('RAILS_ENV') { 'production' }
 
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_timeout Integer(ENV.fetch("PUMA_WORKER_TIMEOUT", "60"))
 
 workers Integer(ENV.fetch('WEB_WORKERS') { 3 })
 threads_count = Integer(ENV.fetch('WEB_THREADS') { 5 })
